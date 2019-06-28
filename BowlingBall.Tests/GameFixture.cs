e24e147f9c1563b655ad0cbbd8aa25f25253c37d
tavisca-apandey <apandey@tavisca.in>
2019-06-28 18:43:13 +0530

    Assignment 3.1

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,80 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+        Game g = new Game();
+
         [Fact]
-        public void DummyTest()
+        public void TestAllZeros()
         {
-            // This is a dummy test that will always pass.
+            rollMultipleChances(20, 0);
+            Assert.Equal(0, g.GetScore());
         }
+
+        [Fact]
+        public void TestAllOnes()
+        {
+            rollMultipleChances(20, 1);
+            Assert.Equal(20, g.GetScore());
+        }
+
+        [Fact]
+        public void TestOneSpare()
+        {
+            Spare();
+            g.Roll(4);
+            rollMultipleChances(17, 0);
+            Assert.Equal(18, g.GetScore());
+        }
+
+        [Fact]
+        public void TestOneStrike()
+        {
+            g.Roll(10);
+            g.Roll(4);
+            g.Roll(5);
+            rollMultipleChances(17, 0);
+            Assert.Equal(28, g.GetScore());
+        }
+
+        [Fact]
+        public void TestPerfectGame()
+        {
+            rollMultipleChances(12, 10);
+            Assert.Equal(300, g.GetScore());
+        }
+
+        [Fact]
+        public void TestNoExtraRoll()
+        {
+            g.Roll(new int[] { 1, 3, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 9, 0 });
+            Assert.Equal(131, g.GetScore());
+        }
+
+        [Fact]
+        public void TestWithSpareThenStrikeInEnd()
+        {
+            g.Roll(new int[] { 1, 3, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 9, 1, 10 });
+            Assert.Equal(143, g.GetScore());
+        }
+
+        [Fact]
+        public void TestWithThreeStrikesInEnd()
+        {
+            g.Roll(new int[] { 1, 3, 7, 3, 10, 1, 7, 5, 2, 5, 3, 8, 2, 8, 2, 10, 10, 10, 10 });
+            Assert.Equal(163, g.GetScore());
+        }
+
+        private void rollMultipleChances(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+                g.Roll(pins);
+        }
+
+        private void Spare()
+        {
+            g.Roll(6);
+            g.Roll(4);
+        }
+
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,16 +4,54 @@ namespace BowlingBall
 {
     public class Game
     {
-        public void Roll(int pins)
-        {
-            throw new NotImplementedException();
-        }
-
+        private int[] rolls = new int[21];
+        private int[] frame = new int[10];
+        int currentRoll = 0;
+        
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int indexFrame = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isSpare(indexFrame))
+                {
+                    score += 10 + rolls[indexFrame + 2];
+                    indexFrame += 2;
+                }
+                else if (isStrike(indexFrame))
+                {
+                    score += 10 + rolls[indexFrame + 1] + rolls[indexFrame + 2];
+                    indexFrame++;
+                }
+                else
+                {
+                    score += rolls[indexFrame] + rolls[indexFrame + 1];
+                    indexFrame += 2;
+                }   
+            }
+            return score;
         }
 
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public void Roll(int[] pins)
+        {
+            for(int indexPins = 0; indexPins < pins.Length; indexPins++)
+                rolls[currentRoll++] = pins[indexPins];
+        }
+        private bool isSpare(int indexFrame)
+        {
+            return rolls[indexFrame] + rolls[indexFrame + 1] == 10;
+        }
+
+        private bool isStrike(int indexFrame)
+        {
+            return rolls[indexFrame] == 10;
+        }
     }
 }
 
