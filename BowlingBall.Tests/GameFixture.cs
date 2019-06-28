6e732b203214f25cccc594a05d21d128005785b8
Aniket <asingla@tavisca.in>
2019-06-28 16:18:00 +0530

    Added Implementation and written tests

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,79 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
-        [Fact]
-        public void DummyTest()
+        private Game g;
+
+        public GameFixture()
         {
-            // This is a dummy test that will always pass.
+            g = new Game();
+        }
+
+        private void rollMany(int nRolls, int pins)
+        {
+            for(int i = 0; i < nRolls; i++)
+            {
+                g.Roll(pins);
+            }
+        }
+        [Fact]
+        public void TestGutterGame()
+        {
+
+            //act
+            rollMany(20, 0);
+
+            int expected = 0;
+            int actual = g.GetScore();
+
+            //assert
+            Assert.Equal(actual, expected);
+
+        }
+
+        [Fact]
+        public void TestAllOnes()
+        {
+            //act
+            rollMany(20, 1);
+            int expected = 20;
+            int actual = g.GetScore();
+
+            //assert
+            Assert.Equal(actual, expected);
+
+        }
+
+        [Fact]
+        public void TestOneSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+            g.Roll(3);
+            rollMany(17, 0);
+
+            Assert.Equal(16, g.GetScore());
+        }
+
+        [Fact]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            g.Roll(3);
+            g.Roll(4);
+            rollMany(16, 0);
+            Assert.Equal(24, g.GetScore());
+        }
+
+        [Fact]
+        public void TestPerfectStrike()
+        {
+            rollMany(12, 10);
+            Assert.Equal(300, g.GetScore());
+        }
+
+        private void RollStrike()
+        {
+            g.Roll(10);
         }
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,16 +4,67 @@ namespace BowlingBall
 {
     public class Game
     {
+        
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            
+            rolls[currentRoll++] = pins;
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int frameIndex = 0;
+            for(int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    score += 10 + StrikeBonus(frameIndex);
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + SpareBonus(frameIndex);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += SumOfBallsInFrame(frameIndex);
+                    frameIndex += 2;
+
+                }
+
+
+                
+            
+            }
+            return score;
         }
 
+        private int SumOfBallsInFrame(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+        private int SpareBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 2];
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return rolls[frameIndex+1] + rolls[frameIndex + 2];
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex] == 10;
+        }
+        private bool IsStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
     }
 }
 
