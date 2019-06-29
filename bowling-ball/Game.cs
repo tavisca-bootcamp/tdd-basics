8fe4e249407e5ed6f3d142df67fc19526182e661
unknown <tjajodia@tavisca.in>
2019-06-29 17:25:30 +0530

    Assigment 1 solved

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,54 @@ namespace BowlingBall
 {
     public class Game
     {
+        int[] roll = new int[21];
+        int rollIndex = 0;
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            if(rollIndex < 20){
+                roll[rollIndex] = pins;
+                rollIndex++;
+            }
+            else if(roll[18] == 10 && rollIndex < 21){
+                roll[rollIndex] = pins;
+                rollIndex++;
+            }
+            else{
+                Console.WriteLine("Game Over You cannot Roll More");
+            }
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int rollingIndex = 0;
+
+            for( int i = 0; i < 10; i++){
+                
+                if(isStrike(rollingIndex)){
+                    score += 10 + roll[rollingIndex + 1] + roll[rollingIndex + 2];
+                    rollingIndex += 1;
+                }
+                else if(isSpare(rollingIndex)){
+                    score += 10 + roll[rollingIndex + 2];
+                    rollingIndex += 2;
+                }
+                else{
+                    score += roll[rollingIndex] + roll[rollingIndex + 1];
+                    rollingIndex += 2; 
+                }
+            }
+
+            return score;
+        }
+
+        public bool isSpare(int index){
+            return roll[index] + roll[index + 1] == 10;
+        }
+
+        public bool isStrike(int index){
+            return roll[index] == 10;
         }
 
     }
