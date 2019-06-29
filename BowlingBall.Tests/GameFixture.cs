00989898de885edd30702afd98f3768c9598354c
vsharma@tavisca.in <vsharma@tavisca.in>
2019-06-30 02:20:07 +0530

    commit to solution

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -1,14 +1,26 @@
 using System;
 using Xunit;
-
 namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+        Game g = new Game();
         [Fact]
-        public void DummyTest()
+        public void TestGameWithOutOfRangePinValues()
         {
-            // This is a dummy test that will always pass.
+            int[] pinsDown = {3,2,9,1,4,-2,-5,19,6,3,10,1,1,5,2,5,9,8,1};
+            for(int i=0; i<pinsDown.Length;i++)
+                {
+                    if(pinsDown[i]<0 || pinsDown[i]>10)
+                    {
+                         Assert.NotInRange(pinsDown[i], 0, 10);
+                         return;
+                    }
+                    else
+                    {
+                         g.Roll(pinsDown[i]);
+                    }
+                }
         }
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,48 @@ namespace BowlingBall
 {
     public class Game
     {
-        public void Roll(int pins)
-        {
-            throw new NotImplementedException();
+        public const int MAX_BALLS = 21;
+        public int[] rolls = new int[MAX_BALLS];
+        int countRoll = 0;
+        private bool IsSpare(int RollIndex) {
+            return rolls[RollIndex] + rolls[RollIndex + 1] == 10;
         }
 
+        private bool IsStrike(int RollIndex)
+        {
+            return rolls[RollIndex] == 10;
+        }
+        public void Roll(int pins)
+        {
+             rolls[countRoll++] = pins;
+        }
+
+        
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int RollIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsSpare(RollIndex))
+                {
+                    score += 10 + rolls[RollIndex + 2];
+                    RollIndex += 2;
+                }
+                else if (IsStrike(RollIndex))
+                {
+                    score += 10 + rolls[RollIndex + 1] + rolls[RollIndex + 2];
+                    RollIndex++;
+                }
+                else
+                {
+                    score += rolls[RollIndex] + rolls[RollIndex + 1];
+                    RollIndex += 2;
+                }
+                
+            }
+            
+            return score;
         }
 
     }
