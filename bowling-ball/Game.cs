a8812d2af5c31933ad1143c7e2c664ecb1487cee
Gursimran Singh <gsingh@tavisca.in>
2019-06-29 10:36:40 +0530

    Completed Game and Tests for BowlingBall

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,61 @@ namespace BowlingBall
 {
     public class Game
     {
+        private int[] rolls = new int[21];
+        private int[] frame = new int[10];
+        int currentRoll = 0;
         public void Roll(int pins)
         {
+            rolls[currentRoll++] = pins;
+            
             throw new NotImplementedException();
+        }
+
+        public void Roll(int[] pins)
+        {
+            for(int i = 0; i < pins.Length; i++)
+            {
+                rolls[currentRoll++] = pins[i];
+            }
         }
 
         public int GetScore()
         {
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else if (isStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+                
+            }
+            
+            return score;
+
             throw new NotImplementedException();
+        }
+
+
+        private bool isSpare(int frameIndex) {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        private bool isStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
         }
 
     }
