1e2fb876dc509c2c9aab3db00ecfeb66a6ada6d7
Saqlain Kadiri <skadiri@tavisca.in>
2019-06-30 00:36:12 +0530

    Completed tdd-basics-01

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -3,12 +3,98 @@ using Xunit;
 
 namespace BowlingBall.Tests
 {
-    public class GameFixture
-    {
-        [Fact]
-        public void DummyTest()
-        {
-            // This is a dummy test that will always pass.
+    public class GameFixture {
+        Game g;
+        public GameFixture() {
+            g = new Game();
         }
+        [Fact]
+        public void TestAllGutters() {
+            NRolls(20, 0);
+            Assert.Equal(0, g.GetScore());
+        }
+        [Fact]
+        public void TestAllOnes() {
+            NRolls(20, 1);
+            Assert.Equal(20, g.GetScore());
+        }
+        [Fact]
+		public void TestFirstFrameSpare(){
+			g.Roll(7); 
+            g.Roll(3);
+            NRolls(18, 2);
+            Assert.Equal(48, g.GetScore());
+		}
+        [Fact]
+        public void TestFirstFrameStrike() {
+            g.Roll(10);
+            NRolls(18, 2);
+            Assert.Equal(50, g.GetScore());
+        }
+        [Fact]
+        public void TestFirstTwoFramesSpares() {
+            g.Roll(7);
+            g.Roll(3);
+            g.Roll(5);
+            g.Roll(5);
+            NRolls(16, 2);
+            Assert.Equal(59, g.GetScore());
+        }
+        [Fact]
+        public void TestFirstTwoFramesStrikes() {
+            g.Roll(10);
+            g.Roll(10);
+            NRolls(16, 2);
+            Assert.Equal(68, g.GetScore());
+        }
+        [Fact]
+        public void TestNineFrameStrikesTenthFrameSpare() {
+            NRolls(9,10);
+            g.Roll(9);
+            g.Roll(1);
+            g.Roll(1);
+            Assert.Equal(270, g.GetScore());
+        }
+        [Fact]
+        public void TestTenthFrameSpareAndStrike() {
+            NRolls(18,0);
+            g.Roll(8);
+            g.Roll(2);
+            g.Roll(10);
+            Assert.Equal(20, g.GetScore());
+        }
+        [Fact]
+        public void TestWithThreeStrikesInEnd() {
+            g.Roll(new int[] { 4, 3, 7, 3, 10, 1, 8, 5, 3, 5, 3, 8, 2, 8, 2, 10, 10, 10, 10 });
+            Assert.Equal(169, g.GetScore());
+        }
+        [Fact]
+        public void TestAllSpares() {
+            NRolls(21, 5);
+            Assert.Equal(150, g.GetScore());
+        }
+        [Fact]
+        public void TestAllStrikes() {
+            NRolls(12, 10);
+            Assert.Equal(300, g.GetScore());
+        }
+        [Fact]
+        public void TestAlternatingStrikeAndSpare() {
+            g.Roll(10);
+            g.Roll(4); g.Roll(6);
+            g.Roll(10);
+            g.Roll(7); g.Roll(3);
+            g.Roll(10);
+            g.Roll(9); g.Roll(1);
+            g.Roll(10);
+            g.Roll(5); g.Roll(5);
+            g.Roll(10);
+            g.Roll(8); g.Roll(2); g.Roll(10);
+            Assert.Equal(200, g.GetScore());
+        }
+        private void NRolls(int n, int pins) {
+            for (int i = 0; i < n; i++)
+                g.Roll(pins);
+        } 
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,19 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BowlingBall
 {
     public class Game
     {
-        public void Roll(int pins)
-        {
-            throw new NotImplementedException();
+        List<int> rolls;
+        public Game() {
+            rolls = new List<int>();
         }
-
-        public int GetScore()
-        {
-            throw new NotImplementedException();
+        public void Roll(int pinsKnocked) {
+            rolls.Add(pinsKnocked);
         }
-
+        public int GetScore() {
+            var bowlingScore = 0;
+            var currentRoll = 0;
+            for (var frame = 0; frame < 10; frame++) {
+                if (IsStrike(currentRoll)) {
+                    bowlingScore += rolls[currentRoll] + rolls[currentRoll + 1] + rolls[currentRoll + 2];
+                    currentRoll++;
+                }
+                else if (IsSpare(currentRoll)) {
+                    bowlingScore += 10 + rolls[currentRoll + 2];
+                    currentRoll += 2;
+                }
+                else {
+                    bowlingScore += rolls[currentRoll] + rolls[currentRoll + 1];
+                    currentRoll += 2;
+                }
+            }
+            return bowlingScore;
+        }
+        public void Roll(int[] pins) {
+            for (int i = 0; i < pins.Length; i++)
+                rolls.Add(pins[i]);
+        }
+        private bool IsSpare(int roll) {
+            return rolls[roll] + rolls[roll + 1] == 10;
+        }
+        private bool IsStrike(int roll) {
+            return rolls[roll] == 10;
+        }
     }
 }
-
