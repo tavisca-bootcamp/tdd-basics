22015ceb61f03f2d7c9a535e2f66efef376b7043
tavisca-dkumar <dkumar@tavisca.in>
2019-06-29 23:57:03 +0530

    intial testcase all zeros

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,9 +5,14 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+        Game obj = new Game();
+
         [Fact]
         public void DummyTest()
         {
+            for (int i = 0; i < 20; i++)
+                obj.Roll(0);
+            Assert.Equal(0, obj.GetScore());
             // This is a dummy test that will always pass.
         }
 
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,8 +4,14 @@ namespace BowlingBall
 {
     public class Game
     {
+        
+        private int [] score = new int[21];
+        private int count=0;
+
         public void Roll(int pins)
         {
+            score[count] = pins;
+            count++;
             throw new NotImplementedException();
         }
 
