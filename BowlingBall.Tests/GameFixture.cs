cc1fd692d499d3ff06a167c47e26729f10f56c16
tavisca-cpadole <51901832+tavisca-cpadole@users.noreply.github.com>
2019-06-29 08:34:19 +0530

    Update GameFixture.cs

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,46 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+        Game game = new Game();
         [Fact]
-        public void DummyTest()
+        public void CanRollGutterGame()
         {
-            // This is a dummy test that will always pass.
+            RollMany(0,20);
+            Assert.Equal(0,game.GetScore());
+        }
+
+        [Fact]
+        public void CanRollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+            RollMany(0, 17);
+            Assert.Equal(16, game.GetScore());
+        }
+
+        [Fact]
+        public void CanRollStrike()
+        {
+            game.Roll(10);
+            game.Roll(5);
+            game.Roll(3);
+            RollMany(0, 16);
+            Assert.Equal(26, game.GetScore());
+        }
+
+        [Fact]
+        public void CanRollPerfectGame()
+        {
+            RollMany(10, 12);
+            Assert.Equal(300, game.GetScore());
+        }
+
+        public void RollMany(int pins, int rolls) {
+            for (int i = 0; i < rolls; i++)
+            {
+                game.Roll(pins);
+            }
         }
     }
 }
