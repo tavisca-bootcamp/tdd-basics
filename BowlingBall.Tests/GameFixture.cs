ec78efc4eb23d24d5a676332aebc9e5acf9ab291
tavisca-akhullar <akhullar@tavisca.in>
2019-06-29 20:41:02 +0530

    done with tdd-basics of assignment-3

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,79 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
-        [Fact]
-        public void DummyTest()
+        private Game game=new Game();
+
+        public void Gamefixture()
         {
-            // This is a dummy test that will always pass.
+            game =new Game();
+        }
+
+        public void RollMany(int nRolls,int pins)
+        {
+            for (int i = 0; i < nRolls; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        [Fact]
+        public void RollGutterGame()
+        {
+           
+            RollMany(20, 0);
+
+            Assert.Equal(game.GetScore(), 0);
+        }
+
+        [Fact]
+        public void RollSpareFirstFrame()
+        {
+            game.Roll(9);
+            game.Roll(1);
+            RollMany(18,1);
+
+            Assert.Equal(game.GetScore(),29);
+        }
+
+        [Fact]
+        public void RollOnes()
+        {
+            
+            RollMany(20, 1);
+
+            Assert.Equal(game.GetScore(), 20);
+        }
+
+        [Fact]
+        public void RollSpareEveryFrame()
+        {
+
+            RollMany(21, 5);
+
+            Assert.Equal(game.GetScore(), 150);
+        }
+
+        [Fact]
+        public void RollPerfectGame()
+        {
+
+            RollMany(12, 10);
+
+            Assert.Equal(game.GetScore(), 300);
+        }
+
+        [Fact]
+        public void RollNineOeSpares()
+        {
+
+            for (int i=0;i<10;i++)
+            {
+                game.Roll(9);
+                game.Roll(1);
+            }
+            game.Roll(9);
+
+            Assert.Equal(game.GetScore(), 190);
         }
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,58 @@ namespace BowlingBall
 {
     public class Game
     {
+        int[] pinFall = new int[21];
+        int rollCounter;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            pinFall[rollCounter] = pins;
+            rollCounter++;
+        }
+
+        bool IsStrike(int frameIndex)
+        {
+            return pinFall[frameIndex] == 10;
+        }
+
+        bool IsSpare(int frameIndex)
+        {
+            return pinFall[frameIndex] + pinFall[frameIndex + 1] == 10;
+        }
+
+        int StrikeBonus(int i)
+        {
+            return pinFall[i + 1] + pinFall[i + 2];
+        }
+
+        int SpareBonus(int i)
+        {
+            return pinFall[i + 2];
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int i = 0;
+            for (int frame=0;frame<10;frame++)
+            {
+                if (IsStrike(i))
+                {
+                    score += 10 + StrikeBonus(i);
+                    i += 1;
+                }
+                else if (IsSpare(i))
+                {
+                    score +=10+ SpareBonus(i);
+                    i += 2;
+                }
+
+                else
+                {
+                    score += pinFall[i] + pinFall[i + 1];
+                    i += 1;
+                }
+            }
+            return score;   
         }
 
     }
