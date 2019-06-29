84646926d928ea1d25b82762a1fbb4f97deb88bb
Neelesh Rajpurohit <nrajpurohit@tavisca.in>
2019-06-29 21:33:41 +0530

    Coded solution for the problem and written Tests for the Solution.

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,62 @@ namespace BowlingBall
 {
     public class Game
     {
+		private int[] rolls = new int[21];
+		private int currentRoll = 0;
+		
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+			this.rolls[currentRoll++] = pins;
         }
 
-        public int GetScore()
+		private bool IsStrike(int frameIndex)
+		{
+			return (rolls[frameIndex] == 10);
+		}
+
+		private bool IsSpare(int frameIndex)
+		{
+			return (rolls[frameIndex] + rolls[frameIndex+1] == 10);
+		}
+
+		private int GetStrikeBonus(int frameIndex)
+		{
+			return (rolls[frameIndex+1] + rolls[frameIndex+2]);
+		}
+
+		private int GetSpareBonus(int frameIndex)
+		{
+			return (rolls[frameIndex + 2]);
+		}
+
+		private int GetFrameSum(int frameIndex)
+		{
+			return (rolls[frameIndex] + rolls[frameIndex+1]);
+		}
+
+		public int GetScore()
         {
-            throw new NotImplementedException();
+			int score = 0;
+			int frameIndex = 0;
+			for (int frame = 0; frame < 10; frame++)
+			{
+				if (IsStrike(frameIndex))
+				{
+					score += 10 + GetStrikeBonus(frameIndex);
+					frameIndex++;
+				}
+				else if (IsSpare(frameIndex))
+				{
+					score += 10 + GetSpareBonus(frameIndex);
+					frameIndex += 2;
+				}
+				else
+				{
+					score += GetFrameSum(frameIndex);
+					frameIndex += 2;
+				}
+			}
+            return score;
         }
 
     }
