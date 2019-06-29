66c09c7420af279e03c2bab5be14100a6f8c9a31
Tanmay Sharma <tsharma@tavisca.in>
2019-06-29 15:13:59 +0530

    Bowling ball

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,43 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
-        [Fact]
-        public void DummyTest()
+        Game game;
+
+        public GameFixture()
         {
-            // This is a dummy test that will always pass.
+            game = new Game();
         }
+
+
+        [Fact]
+        public void canScoreGutterGame()
+        {
+            int[] a = { 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0 };
+            game.Roll(a);
+            Assert.Equal(0, game.GetScore());
+        }
+
+        [Fact]
+        public void canScoreGameOfOnes()
+        {
+            int[] a = { 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1 };
+            game.Roll(a);
+            Assert.Equal(20, game.GetScore());
+        }
+
+        [Fact]
+        public void canScoreSpareFollowedByThree()
+        {
+            int[] a = { 5,5, 3,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0 };
+            game.Roll(a);
+            Assert.Equal(16, game.GetScore());
+        }
+
+        [Fact]
+        public void canScoreStrikeFollowedByThreeThenThree()
+        {
+            int[] a = { 10, 3,3, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0 };
+        }
+
     }
 }
