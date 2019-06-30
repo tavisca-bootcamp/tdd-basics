675b0342d20c798bdbf1c9cf2d872a7171867416
rajat <ryadav@tavisca.in>
2019-06-30 16:42:41 +0530

    Added classes Frame.cs

---
+++ bowling-ball/Frame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BowlingBall
+{
+    public class Frame
+    {
+        private int roll1;
+        private int roll2;
+        public Frame(int roll1,int roll2)
+        {
+            this.roll1 = roll1;
+            this.roll2 = roll2;
+        }
+        public int Roll1 { get { return roll1; } }
+        public int Roll2 { get { return roll2; } }
+    }
+}
