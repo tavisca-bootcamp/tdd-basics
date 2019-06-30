f59089c6bd10116f8dc837d0c42743fdae8a9b56
tavisca-bnaveen <bnaveen@tavisca.in>
2019-06-30 11:21:27 +0530

    assignment3_bowling

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -3,12 +3,28 @@ using Xunit;
 
 namespace BowlingBall.Tests
 {
+    [TestClass]
     public class GameFixture
     {
-        [Fact]
+        [TestMethod]
         public void DummyTest()
         {
-            // This is a dummy test that will always pass.
+            Game game = new Game();
+            int k;
+            int[] a = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+            for (k = 0; k < a.Length; k++)
+                game.Roll(a[k]);
+            Assert.Equal("300", game.GetScore());
+        }
+        [TestMethod]
+        public void DummyTest1()
+        {
+            Game game1 = new Game();
+            int k;
+            int[] a = { 10,9,1,5,5,7,2,10,10,10,9,0,8,2,9,1,10};
+            for (k = 0; k < a.Length; k++)
+                game1.Roll(a[k]);
+            Assert.Equal("187", game1.GetScore());
         }
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,19 +1,49 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace BowlingBall
 {
     public class Game
     {
+        public int score = 0, current = 0;
+        public int rounds = 0;
+        public int[] Roll_values = new int[21];
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            Roll_values[current++] = pins;
         }
-
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int i, j;
+            for (i = 0; i < current; i++)
+            {
+                if (rounds < 9)
+                {
+                    if (Roll_values[i] == 10)
+                    {
+                        score += Roll_values[i] + Roll_values[i + 1] + Roll_values[i + 2];
+                        rounds++;
+                    }
+                    else
+                    {
+                        score += Roll_values[i];
+                        i++;
+                        if (Roll_values[i] == 10 - Roll_values[i - 1])
+                            score += Roll_values[i] + Roll_values[i + 1];
+                        else
+                            score += Roll_values[i];
+                        rounds++;
+                    }
+                }
+                else
+                {
+                    for (j = i; j < current; j++)
+                        score += Roll_values[j];
+                    break;
+                }
+            }
+            return score;
+            //throw new NotImplementedException();
         }
-
     }
 }
 
