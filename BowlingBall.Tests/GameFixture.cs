47b257e7615b1ab680f5f0a32d30a0a3ce505502
tavisca-dgupta <51922254+tavisca-dgupta@users.noreply.github.com>
2019-06-30 13:57:20 +0530

    Update GameFixture.cs

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -6,9 +6,49 @@ namespace BowlingBall.Tests
     public class GameFixture
     {
         [Fact]
-        public void DummyTest()
+        public void AllZeroTest()
         {
-            // This is a dummy test that will always pass.
+            int[] roll = new int[] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+            int expected = 0;
+            int actual = GetActualScore(roll);
+            Asset.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AllOneTest()
+        {
+            int[] roll = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int expected = 0;
+            int actual = GetActualScore(roll);
+            Asset.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StrikeSpareTest()
+        {
+            int[] roll = new int[] { 10, 9, 1, 5, 5, 7, 2, 10, 10, 10, 9, 0, 8, 2, 9, 1, 10 };
+            int expected = 0;
+            int actual = GetActualScore(roll);
+            Asset.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AllSpareTest()
+        {
+            int[] roll = new int[] { 5,5, 9, 1, 5, 5, 7, 3, 3,7, 4,6, 5,5, 9, 1, 8, 2, 9, 1, 1 };
+            int expected = 0;
+            int actual = GetActualScore(roll);
+            Asset.Equal(expected, actual);
+        }
+
+        public static int GetActualScore(int[] roll)
+        {
+            var game = new Game();
+            foreach(int rollValue in roll)
+            {
+                game.Roll(rollValue);
+            }
+            return game.GetScore();
         }
     }
 }
