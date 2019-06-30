9189db17503289aac0295e21cf7a52192ab87796
tavisca-dgupta <51922254+tavisca-dgupta@users.noreply.github.com>
2019-06-30 13:59:35 +0530

    Update Game.cs

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,15 +4,51 @@ namespace BowlingBall
 {
     public class Game
     {
+        private static int[] bowlingRoll = new int[21];
+        private static int currentRoll = 0;
+        private static int score = 0;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            bowlingRoll[currentRoll] = pins;
+            currentRoll++;
+            //throw new NotImplementedException();
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int rollCount = 0;
+            while (rollCount < bowlingRoll.Length - 1)
+            {
+                if (bowlingRoll[rollCount] == 10)
+                {
+                    score = score + GetStrikeScore(rollCount);
+                    rollCount++;
+                }
+                else if ((bowlingRoll[rollCount] + bowlingRoll[rollCount + 1]) == 10)
+                {
+                    score = score + GetSpareScore(rollCount);
+                    rollCount = rollCount + 2;
+                }
+                else
+                {
+                    score = score + bowlingRoll[rollCount] + bowlingRoll[rollCount + 1];
+                    rollCount = rollCount + 2;
+                }
+            }
+            return score;
         }
+        public int GetStrikeScore(int rollCount)
+        {
+            int strikeScore = bowlingRoll[rollCount] + bowlingRoll[rollCount + 1] + bowlingRoll[rollCount + 2];
+            return strikeScore;
+        }
+        public int GetSpareScore(int rollCount)
+        {
+            int spareScore = bowlingRoll[rollCount] + bowlingRoll[rollCount + 1] + bowlingRoll[rollCount + 2];
+            return spareScore;
+        }
+
+        //throw new NotImplementedException();
 
     }
 }
