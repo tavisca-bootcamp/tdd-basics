68bbb564067ba9eb07b5d5450e48db24612f3a32
tavisca-sniyazi <sniyazi@tavisca.in>
2019-06-30 19:39:51 +0530

    done assigment3

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,75 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
-        [Fact]
-        public void DummyTest()
+        private Game game = new Game();
+
+        internal void RollMany(int nRolls, int pins)
         {
-            // This is a dummy test that will always pass.
+            for (int i = 0; i < nRolls; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        [Fact]
+        public void RollGutterGame()
+        {
+
+            RollMany(20, 0);
+
+            Assert.Equal(0,game.GetScore());
+        }
+
+        [Fact]
+        public void RollSpareFirstFrame()
+        {
+            game.Roll(9);
+            game.Roll(1);
+            RollMany(18, 1);
+
+            Assert.Equal(29,game.GetScore());
+        }
+
+        [Fact]
+        public void RollOnes()
+        {
+
+            RollMany(20, 1);
+
+            Assert.Equal(20,game.GetScore());
+        }
+
+        [Fact]
+        public void RollSpareEveryFrame()
+        {
+
+            RollMany(21, 5);
+
+            Assert.Equal(150,game.GetScore());
+        }
+
+        [Fact]
+        public void RollPerfectGame()
+        {
+
+            RollMany(12, 10);
+
+            Assert.Equal(300,game.GetScore());
+        }
+
+        [Fact]
+        public void RollNineOeSpares()
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                game.Roll(9);
+                game.Roll(1);
+            }
+            game.Roll(9);
+
+            Assert.Equal(190,game.GetScore());
         }
     }
 }
+
