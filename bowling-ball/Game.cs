29962795d96ecd33163225d9556d167ca68397aa
rajat <ryadav@tavisca.in>
2019-06-30 16:48:00 +0530

    Frames taken as object Array of Frame object created

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,22 +1,35 @@
 ﻿using System;
-
+using System.Collections;
 namespace BowlingBall
 {
     public class Game
     {
         private int score = 0;
+        private ArrayList frames;
+
+        public Game()
+        {
+            frames = new ArrayList();
+        }
+
         public void RollFrame(int roll1, int roll2)
         {
-            score += (roll1 + roll2);
+            frames.Add(new Frame(roll1, roll2));
+        }
+
+        public void RollSpare(int roll1,int roll2)
+        {
+
         }
 
         public int GetScore()
         {
-            return score;
-        }
-        public void RollSpare(int roll1,int roll2)
-        {
+            foreach(Frame frame in frames)
+            {
+                score += (frame.Roll1 + frame.Roll2);
+            }
 
+            return score;
         }
 
     }
