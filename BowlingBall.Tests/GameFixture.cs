131ae74bccc31983d583b46b53cb32f29d47ed6c
visharma <visharma@tavisca.in>
2019-06-30 07:19:42 +0530

    Solved Assignment 1 of Unit Testing Successfully

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,179 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
-        [Fact]
-        public void DummyTest()
+        private Game game;
+        public GameFixture()
         {
-            // This is a dummy test that will always pass.
+            game = new Game();
+        }
+
+        [Fact]
+        public void BasicTestForZeroScoring()
+        {
+            // Arrange
+            int actual;
+            int expected;
+
+            // Act
+            rollMultipleTimes(20, 0);
+            actual = game.GetScore();
+            expected = 0;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void testAllOnes()
+        {
+            // Arrange
+            int actual;
+            int expected;
+
+            // Act
+            rollMultipleTimes(20, 1);
+            actual = game.GetScore();
+            expected = 20;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void testOneSpare()
+        {
+            // Arrange
+            int actual;
+            int expected;
+
+            // Act
+            rollSpare();
+            game.Roll(3);
+            rollMultipleTimes(17, 0);
+            actual = game.GetScore();
+            expected = 16;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void testOneStrike()
+        {
+            // Arrange
+            int actual;
+            int expected;
+
+            // Act
+            rollStrike();
+            game.Roll(3);
+            game.Roll(4);
+            rollMultipleTimes(16, 0);
+            actual = game.GetScore();
+            expected = 24;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void testPerfectGame()
+        {
+            // Arrange
+            int actual;
+            int expected;
+
+            // Act
+
+            rollMultipleTimes(12, 10);
+            actual = game.GetScore();
+            expected = 300;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void test1()
+        {
+            // Arrange
+            int actual;
+            int expected;
+
+            // Act
+            int[] rolls = { 10, 9, 1, 5, 5, 7, 2, 10, 10, 10, 9, 0, 8, 2, 9, 1, 10 };
+            makeRolls(rolls);
+            actual = game.GetScore();
+            expected = 187;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void test2()
+        {
+            // Arrange
+            int actual;
+            int expected;
+
+            // Act
+            int[] rolls = { 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0, 9, 0 };
+            makeRolls(rolls);
+            actual = game.GetScore();
+            expected = 90;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void test3()
+        {
+            // Arrange
+            int actual;
+            int expected;
+
+            // Act
+            int[] rolls = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+            makeRolls(rolls);
+            actual = game.GetScore();
+            expected = 150;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        /*
+            Utility functions for testing.
+        */
+        private void rollMultipleTimes(int numberOfTimes, int pins)
+        {
+            for (int i = 0; i < numberOfTimes; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        private void makeRolls(int[] rolls)
+        {
+            for (int i = 0; i < rolls.Length; i++)
+            {
+                game.Roll(rolls[i]);
+            }
+        }
+
+        private void rollStrike()
+        {
+            game.Roll(10);
+        }
+
+        private void rollSpare()
+        {
+            game.Roll(7);
+            game.Roll(3);
         }
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,16 +4,66 @@ namespace BowlingBall
 {
     public class Game
     {
+        const int MAXIMUM_ROLL_SIZE = 21;
+        const int MAXIMUM_FRAME_SIZE = 10;
+        int[] rolls = new int[MAXIMUM_ROLL_SIZE];
+        int[] frameScore = new int[MAXIMUM_FRAME_SIZE + 1];
+        int currentRoll = 0;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            rolls[currentRoll] = pins;
+            currentRoll++;
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int rollsIndex = 0;
+            frameScore[0] = 0;
+            for (int frameIndex = 1; frameIndex <= MAXIMUM_FRAME_SIZE; frameIndex++)
+            {
+                if (isStrike(rollsIndex))
+                {
+                    frameScore[frameIndex] = frameScore[frameIndex - 1] + 10 + strikeBonus(rollsIndex);
+                    rollsIndex++;
+                }
+                else if (isSpare(rollsIndex))
+                {
+                    frameScore[frameIndex] = frameScore[frameIndex - 1] + 10 + spareBonus(rollsIndex);
+                    rollsIndex += 2;
+                }
+                else
+                {
+                    frameScore[frameIndex] = frameScore[frameIndex - 1] + sumOfBallsInTheFrame(rollsIndex);
+                    rollsIndex += 2;
+                }
+            }
+            return frameScore[frameScore.Length - 1];
         }
 
+        private bool isSpare(int rollsIndex)
+        {
+            return rolls[rollsIndex] + rolls[rollsIndex + 1] == 10;
+        }
+
+        private int spareBonus(int rollsIndex)
+        {
+            return rolls[rollsIndex + 2];
+        }
+
+        private int sumOfBallsInTheFrame(int rollsIndex)
+        {
+            return rolls[rollsIndex] + rolls[rollsIndex + 1];
+        }
+
+        private int strikeBonus(int rollsIndex)
+        {
+            return rolls[rollsIndex + 1] + rolls[rollsIndex + 2];
+        }
+
+        private bool isStrike(int rollsIndex)
+        {
+            return rolls[rollsIndex] == 10;
+        }
     }
 }
 
