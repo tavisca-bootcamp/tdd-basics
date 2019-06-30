ff535049f4a3c36c88ec5c63913453cef6e21195
Stark <shravanramdurg45@gmail.com>
2019-06-30 14:41:28 +0530

    TDD basics #1

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,71 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
-        [Fact]
-        public void DummyTest()
+        private Game game = new Game();
+
+        protected void setUp() 
         {
-            // This is a dummy test that will always pass.
+            game = new Game();
         }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        [Fact]
+        public void TestGutterGame()
+        {
+            RollMany(20, 0);
+            Assert.Equal(0, game.GetScore());
+        }
+
+        [Fact]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+            Assert.Equal(20, game.GetScore());
+        }
+
+        [Fact]
+        public void TestOneSpare()
+        {
+            RollSpare();      //Spare
+            game.Roll(3);
+            RollMany(17, 0);
+            Assert.Equal(16, game.GetScore());
+        }
+
+        [Fact]
+        public void TestOneStrike() 
+        {
+            RollStrike(); // strike
+            game.Roll(3);
+            game.Roll(4);
+            RollMany(16, 0);
+            Assert.Equal(24, game.GetScore());
+        }
+
+        [Fact]
+        public void TestPerfectGame() 
+        {
+            RollMany(12,10);
+            Assert.Equal(300, game.GetScore());
+        }
+
+        private void RollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            game.Roll(10);
+        }
+
     }
 }
