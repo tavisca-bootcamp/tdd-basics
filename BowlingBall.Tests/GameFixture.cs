f78c5044706d4c82d7968915eb46583ef2585f3e
rajat <ryadav@tavisca.in>
2019-06-30 16:24:45 +0530

    Removed duplicacy and refactored code

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -4,27 +4,33 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+        Game game=null;
+
+        public GameFixture()
+        {
+            game = new Game();
+        }
 
         [Fact]
         public void AllGutterBalls()
         {
-            var game = new Game();
-            for(int frame = 1; frame <= 10; frame++)
-            {
-                game.RollFrame(0, 0);
-            }
+            RollFrames(0, 0, 10);
             Assert.Equal(0, game.GetScore());
         }
 
         [Fact]
         public void All2sShouldScore40()
         {
-            var game = new Game();
-            for(int frame = 1; frame <= 10; frame++)
-            {
-                game.RollFrame(2, 2);
-            }
+            RollFrames(2, 2,10);
             Assert.Equal(40, game.GetScore());
+        }
+
+        private void RollFrames(int roll1,int roll2,int numberOfFrames)
+        {
+            for(int frame = 0; frame < numberOfFrames; frame++)
+            {
+                game.RollFrame(roll1, roll2);
+            }
         }
     }
 }
