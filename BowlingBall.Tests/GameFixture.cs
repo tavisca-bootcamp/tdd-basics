ac7dcdbe80a512de626fe1eff1c016569490d461
Rameez Parkar <rparkar@tavisca.in>
2019-06-30 18:40:34 +0530

    Completed tdd-basics-01

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,78 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
-        [Fact]
-        public void DummyTest()
+        Game game = new Game();
+        private void RollMany(int n, int pins)
         {
-            // This is a dummy test that will always pass.
+            for(int i=0; i<n; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        private void RollMany(int[] pins)
+        {
+            for(int i=0; i<pins.Length; i++)
+            {
+                game.Roll(pins[i]);
+            }
+        }
+
+        [Fact]
+        public void TestForAllZeros()
+        {
+            RollMany(20, 0);
+            Assert.Equal(0, game.GetScore());
+        }
+
+        [Fact]
+        public void TestForAllOnes()
+        {
+            RollMany(20, 1);
+            Assert.Equal(20, game.GetScore());
+        }
+
+        [Fact]
+        public void TestOneSpare()
+        {
+            game.Roll(4);
+            game.Roll(6);
+            game.Roll(3);
+            RollMany(17, 0);
+            Assert.Equal(16, game.GetScore());
+        }
+
+        [Fact]
+        public void TestOneStrike()
+        {
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(4);
+            RollMany(16, 0);
+            Assert.Equal(24, game.GetScore());
+        }
+
+        [Fact]
+        public void TestPerfectGame()
+        {
+            RollMany(12, 10);
+            Assert.Equal(300, game.GetScore());
+        }
+
+        [Fact]
+        public void TestRandomGame()
+        {
+            int[] pins = { 5, 4, 9, 1, 7, 3, 2, 0, 10, 10, 6, 2, 3, 3, 10, 3, 7, 6 };
+            RollMany(pins);
+            Assert.Equal(134, game.GetScore());
+        }
+
+        [Fact]
+        public void TestProblemStatementGame()
+        {
+            int[] pins = { 10, 9, 1, 5, 5, 7, 2, 10, 10, 10, 9, 0, 8, 2, 9, 1, 10 };
+            RollMany(pins);
+            Assert.Equal(187, game.GetScore());
         }
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,62 @@ namespace BowlingBall
 {
     public class Game
     {
+        private const int maxPossibleRolls = 21;
+        private const int maxPossibleFrames = 10;
+        private int[] rollScore = new int[maxPossibleRolls];
+        private int[] frameScore = new int[maxPossibleFrames];
+        private int rollNumber = 0;
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            rollScore[rollNumber] = pins;
+            rollNumber++;
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int currentRoll = 0;
+
+            for(int frame=0; frame<maxPossibleFrames; frame++)
+            {
+                if(CheckForStrike(currentRoll) == true)
+                {
+                    score += rollScore[currentRoll] + rollScore[currentRoll + 1] + rollScore[currentRoll + 2];
+                    frameScore[frame] = score;
+                    currentRoll++;
+                }
+                else if (CheckForSpare(currentRoll) == true)
+                {
+                    score += rollScore[currentRoll] + rollScore[currentRoll + 1] + rollScore[currentRoll + 2];
+                    frameScore[frame] = score;
+                    currentRoll += 2;
+                }
+                else
+                {
+                    score += rollScore[currentRoll] + rollScore[currentRoll + 1];
+                    frameScore[frame] = score;
+                    currentRoll += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool CheckForStrike(int currentRoll)
+        {
+            if (rollScore[currentRoll] == 10)
+                return true;
+            else
+                return false;
+        }
+
+        private bool CheckForSpare(int currentRoll)
+        {
+            if ((rollScore[currentRoll] + rollScore[currentRoll + 1]) == 10)
+                return true;
+            else
+                return false;
         }
 
     }
