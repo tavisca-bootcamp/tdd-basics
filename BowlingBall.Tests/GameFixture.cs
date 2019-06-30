71898f09f6d2c637f9de3a2bec0ec42302a118bb
rajat <ryadav@tavisca.in>
2019-06-30 16:07:08 +0530

    Added method for all rolls are 2's in all frames

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -15,5 +15,16 @@ namespace BowlingBall.Tests
             }
             Assert.Equal(0, game.GetScore());
         }
+
+        [Fact]
+        public void All2sShouldScore40()
+        {
+            var game = new Game();
+            for(int frame = 1; frame <= 10; frame++)
+            {
+                game.RollFrame(2, 2);
+            }
+            Assert.Equal(40, game.GetScore());
+        }
     }
 }
