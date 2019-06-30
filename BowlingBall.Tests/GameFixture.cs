101fff356ce585699a514f33c56fe6f6d5f21aaa
SHRILATA MONDAL <51924756+tavisca-smandal@users.noreply.github.com>
2019-06-30 21:43:28 +0530

    TDD-Basics Completed

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,70 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
-        [Fact]
-        public void DummyTest()
+        private Game game;
+        public GameFixture()
         {
-            // This is a dummy test that will always pass.
+            game = new Game();
+        }
+
+        public void CompleteRollSet(int[] roll)
+        {
+            for (int i = 0; i < roll.Length; i++)
+            {
+                game.Roll(roll[i]);
+            }
+        }
+
+        public void MultipleRoll(int roll, int pins)
+        {
+            for (int i = 0; i < roll; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+        [Fact]
+        public void TestGameClass()
+        {
+            Assert.IsType<Game>(game);
+        }
+
+        [Fact]
+        public void TestSpare()
+        {
+            game.Roll(7);
+            game.Roll(3);
+            Assert.Equal(10, game.GetScore());
+        }
+
+        [Fact]
+        public void TestStrike()
+        {
+            game.Roll(10);
+            Assert.Equal(10, game.GetScore());
+        }
+
+        [Fact]
+        public void TestMissingRoll()
+        {
+            game.Roll(0);
+            Assert.Equal(0, game.GetScore());
+        }
+
+
+        [Fact]
+        public void TestCompleteRollSet()
+        {
+            int[] roll = { 10, 9, 1, 5, 5, 7, 2, 10, 10, 10, 9, 0, 8, 2, 9, 1, 10 };
+            CompleteRollSet(roll);
+            Assert.Equal(187, game.GetScore());
+        }
+
+        [Fact]
+        public void TestPerfectGame()
+        {
+            MultipleRoll(12, 10);
+            Assert.Equal(300, game.GetScore());
         }
     }
 }
