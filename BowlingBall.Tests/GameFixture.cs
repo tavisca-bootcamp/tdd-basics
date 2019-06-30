92afa71d3e1b6ab9b62a56592485210e16f052d1
tavisca-dkumar <dkumar@tavisca.in>
2019-06-30 09:16:28 +0530

    added testcase sparezerosrike
    starts with a spare in middle gutter balls and ends with strike

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -8,37 +8,46 @@ namespace BowlingBall.Tests
         Game obj = new Game();
 
         [Fact]
-        public void DummyTest()
+        public void AllZeros()
         {
             for (int i = 0; i < 20; i++)
                 obj.Roll(0);
             Assert.Equal(0, obj.GetScore());
-            // This is a dummy test that will always pass.
+            
         }
 
         [Fact]
-        public void DummyTest1()
+        public void AllOnes()
         {
-            // This is a dummy test that will always pass.
+            
             for (int i = 0; i < 20; i++)
                 obj.Roll(1);
             Assert.Equal(20, obj.GetScore());
         }
         [Fact]
-        public void DummyTest2()
+        public void AllSpares()
         {
-            // This is a dummy test that will always pass.
+            
             for (int i = 0; i < 21; i++)
                 obj.Roll(5);
             Assert.Equal(150, obj.GetScore());
         }
         [Fact]
-        public void DummyTest3()
+        public void AllStrikes()
         {
-            // This is a dummy test that will always pass.
+           
             for (int i = 0; i < 12; i++)
                 obj.Roll(10);
             Assert.Equal(300, obj.GetScore());
+        }
+        [Fact]
+        public void SpareZeroStrike() //starts with a spare in middle gutter balls and ends with strike
+        {
+            int[] testcase = new int[20] { 5, 5, 1, 2, 7, 3, 5, 0, 0, 0, 4, 3, 2, 1, 10, 9, 0, 10, 5, 1 };
+
+            for (int i = 0; i < 20; i++)
+                obj.Roll(testcase[i]);
+            Assert.Equal(88, obj.GetScore());
         }
 
     }
