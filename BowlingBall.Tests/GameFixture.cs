e9c4595e7b9a3b5f53d37e6e0147ccead8b24a43
Akshaya H <ahari@tavisca.in>
2019-07-01 02:35:04 +0530

    Bowling Score Generator and Test Cases

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,89 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
-        [Fact]
-        public void DummyTest()
+        private Game g;
+        public GameFixture()
         {
-            // This is a dummy test that will always pass.
+            g = new Game();
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+                g.Roll(pins);
+        }
+
+        [Fact]
+         public void TestGutterGame()
+        {
+            RollMany(20, 0);
+            Assert.Equal(0, g.GetScore());
+
+        }
+
+        [Fact]
+        public void TestAllOnes()
+        {
+           
+            RollMany(20, 1);
+            Assert.Equal(20, g.GetScore());
+        }
+
+        [Fact]
+        public void TestOneSpare()
+        {
+            
+            RollSpare();
+            g.Roll(3);
+            RollMany(17, 0);
+            Assert.Equal(16, g.GetScore());
+        }
+        
+        [Fact]
+        public void TestOneStrike()
+        {
+          
+            RollStrike();
+            g.Roll(3);
+            g.Roll(4);
+            RollMany(16, 0);
+            Assert.Equal(24, g.GetScore());
+        }
+
+        [Fact]
+        public void TestPerfectGame()
+        {
+            g = new Game();
+            RollMany(12, 10);
+            Assert.Equal(300, g.GetScore());
+        }
+
+        [Fact]
+        public void TypicalGame()
+        {
+            g.Roll(10);
+            g.Roll(9); g.Roll(1);
+            g.Roll(5); g.Roll(5);
+            g.Roll(7); g.Roll(2);
+            g.Roll(10);
+            g.Roll(10);
+            g.Roll(10);
+            g.Roll(9); g.Roll(0);
+            g.Roll(8); g.Roll(2);
+            g.Roll(9); g.Roll(1); g.Roll(10);
+
+            Assert.Equal(187, g.GetScore());
+        }
+
+        private void RollStrike()
+        {
+            g.Roll(10);
+        }
+
+        private void RollSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
         }
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,19 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BowlingBall
 {
     public class Game
     {
+        private int[] rolls = new int[21];
+        private int rollCount = 0;
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            rolls[rollCount++] = pins;
         }
-
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int rollIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(rollIndex))
+                {
+                    score += 10 + StrikeBonus(rollIndex);
+                    rollIndex += 1;
+
+                }
+                else if (IsSpare(rollIndex))
+                {
+                    score += 10 + rolls[rollIndex + 2];
+                    rollIndex += 2;
+                }
+                else
+                {
+                    score += SumOfPinsInFrame(rollIndex);
+                    rollIndex += 2;
+                }
+            }
+            return score;
         }
-        //hello
+
+        private bool IsStrike(int rollIndex)
+        {
+            return rolls[rollIndex] == 10;
+        }
+
+        private int SumOfPinsInFrame(int rollIndex)
+        {
+           return rolls[rollIndex] + rolls[rollIndex + 1];
+        }
+
+        private int StrikeBonus(int rollIndex)
+        {
+            return rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        }
+
+        private bool IsSpare(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+        }
     }
 }
 
