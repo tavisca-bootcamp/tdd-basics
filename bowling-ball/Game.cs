567aa2d174fed15b5405612b2eef1b513fb89418
rajat <ryadav@tavisca.in>
2019-06-30 16:11:05 +0530

    Added score variable in Game class

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,15 @@ namespace BowlingBall
 {
     public class Game
     {
+        private int score = 0;
         public void RollFrame(int roll1, int roll2)
         {
-           
+            score += (roll1 + roll2);
         }
 
         public int GetScore()
         {
-            return 0;
+            return score;
         }
 
     }
