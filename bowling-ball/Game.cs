a439d1d9b7c72f16dcb1cbda3772dd99751bedc2
Raunak singh <51930237+tavisca-rsingh@users.noreply.github.com>
2019-06-30 19:01:12 +0530

    bowlingBall

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,62 @@ namespace BowlingBall
 {
     public class Game
     {
+        int[] frame = new int[21];
+        int currentRoll;
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            frame[currentRoll++] = pins;
         }
 
-        public int GetScore()
+        public int getScore()
         {
-            throw new NotImplementedException();
+            var score = 0;
+            var roll = 0;
+
+            for (var frameNumber = 1; frameNumber <= 10; frameNumber++)
+            {
+                if (isStrike(roll))
+                {
+                    score += 10 + strikeBonus(roll);
+                    roll++;
+                }
+                else if (isSpare(roll))
+                {
+                    score += 10 + spareBonus(roll);
+                    roll += 2;
+                }
+                else
+                {
+                    score += sumOfBallsInFrame(roll);
+                    roll += 2;
+                }
+            }
+            return score;
+        }
+        private int sumOfBallsInFrame(int roll)
+        {
+            return frame[roll] + frame[roll + 1];
+        }
+
+        private int spareBonus(int roll)
+        {
+            return frame[roll + 2];
+        }
+
+        private int strikeBonus(int roll)
+        {
+            return frame[roll + 1] + frame[roll + 2];
+        }
+
+        private bool isStrike(int roll)
+        {
+            return frame[roll] == 10;
+        }
+
+        private bool isSpare(int roll)
+        {
+            return frame[roll] + frame[roll + 1] == 10;
         }
 
     }
