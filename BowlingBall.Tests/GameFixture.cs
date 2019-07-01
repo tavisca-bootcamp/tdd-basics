f817b01989d97526086b6d3f9c3df60f9b059dca
raggarwal <raggarwal@tavisca.in>
2019-07-01 11:10:47 +0530

    Added program

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,91 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+        Game g = new Game();
+
         [Fact]
-        public void DummyTest()
+        public void ForAllZeroes()
         {
-            // This is a dummy test that will always pass.
+            for (int i = 0; i < 20; i++)
+            {
+                g.Roll(0);
+            }
+            Assert.Equal(0, g.GetScore());
+
+        }
+
+        [Fact]
+        public void ForAllOnes()
+        {
+
+            for (int i = 0; i < 20; i++)
+            {
+                g.Roll(1);
+            }
+            Assert.Equal(20, g.GetScore());
+        }
+
+        [Fact]
+        public void ForOneSpare()
+        {
+
+            g.Roll(6);
+            g.Roll(4);
+            g.Roll(3);
+            for (int i = 0; i < 17; i++)
+            {
+                g.Roll(0);
+            }
+            Assert.Equal(16, g.GetScore());
+        }
+
+        [Fact]
+        public void ForOneStrike()
+        {
+
+            g.Roll(10);
+            g.Roll(3);
+            g.Roll(4);
+            for (int i = 0; i < 16; i++)
+            {
+                g.Roll(2);
+            }
+            Assert.Equal(56, g.GetScore());
+        }
+
+        [Fact]
+        public void ForAllStrikes()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                g.Roll(10);
+            }
+            Assert.Equal(300, g.GetScore());
+        }
+
+        [Fact]
+        public void ForAllSpares()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Spare();
+            }
+            g.Roll(6);
+            Assert.Equal(160, g.GetScore());
+        }
+        [Fact]
+        public void ForStrikeAndSpareInEnd()
+        {
+            g.Roll(new int[] { 5, 2, 4, 5, 10, 2, 6, 4, 5, 7, 1, 8, 2, 9, 1, 10, 8, 2, 10 });
+            Assert.Equal(138, g.GetScore());
+        }
+
+
+        private void Spare()
+        {
+           
+            g.Roll(6);
+            g.Roll(4);
         }
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,15 +4,50 @@ namespace BowlingBall
 {
     public class Game
     {
+        private int[] roll = new int[21];
+        private int currentroll = 0;
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            roll[currentroll++] = pins;
+        }
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                roll[currentroll++] = pins[i];
+            }
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int index = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (roll[index] == 10)
+                {
+                    score += 10 + roll[index + 1] + roll[index + 2];
+                    index += 1;
+
+                }
+                else if (roll[index] + roll[index + 1] == 10)
+                {
+                    score += 10 + roll[index + 2];
+                    index += 2;
+                }
+                else
+                {
+                    score += roll[index] + roll[index + 1];
+                    index += 2;
+                }
+            }
+            return score;
         }
+
+
+        //throw new NotImplementedException();
+
 
     }
 }
