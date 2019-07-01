442e18678cf4198c9163fc8701417f1190650295
tavisca-akhullar <akhullar@tavisca.in>
2019-07-01 14:42:06 +0530

    comments and additional stuffs added

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -7,6 +7,7 @@ namespace BowlingBall.Tests
     {
         private Game game=new Game();
 
+        
         public void RollMany(int nRolls,int pins)
         {
             for (int i = 0; i < nRolls; i++)
@@ -15,6 +16,7 @@ namespace BowlingBall.Tests
             }
         }
 
+        //test when no pins is knocked
         [Fact]
         public void RollGutterGame()
         {
@@ -24,6 +26,8 @@ namespace BowlingBall.Tests
             Assert.Equal(game.GetScore(), 0);
         }
 
+        
+        //test when first frame is spare and on every attempt case one pin is knocked
         [Fact]
         public void RollSpareFirstFrame()
         {
@@ -35,6 +39,8 @@ namespace BowlingBall.Tests
         }
 
         [Fact]
+
+        //test when in every attempt only one pin is knocked
         public void RollOnes()
         {
             
@@ -43,6 +49,8 @@ namespace BowlingBall.Tests
             Assert.Equal(game.GetScore(), 20);
         }
 
+
+        //test when spare occur on every frame
         [Fact]
         public void RollSpareEveryFrame()
         {
@@ -52,15 +60,17 @@ namespace BowlingBall.Tests
             Assert.Equal(game.GetScore(), 150);
         }
 
+        //test when strike occurs on every attempt
         [Fact]
         public void RollPerfectGame()
         {
 
             RollMany(12, 10);
-
             Assert.Equal(game.GetScore(), 300);
         }
 
+        //test when every frame has 9 pins knocked in first attempt and 1 pin knocked at second attempt
+        //i.e on every frame spare occur
         [Fact]
         public void RollNineOeSpares()
         {
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,56 +4,71 @@ namespace BowlingBall
 {
     public class Game
     {
+        //array to store number of pins knocked in single roll
         int[] pinFall = new int[21];
         int rollCounter;
+
+        //Method when ball is rolled
         public void Roll(int pins)
         {
             pinFall[rollCounter] = pins;
             rollCounter++;
         }
 
+        //Method to check for strike 
         bool IsStrike(int frameIndex)
         {
             return pinFall[frameIndex] == 10;
         }
 
+        //Method to check for spare 
         bool IsSpare(int frameIndex)
         {
             return pinFall[frameIndex] + pinFall[frameIndex + 1] == 10;
         }
 
+        //Method to evaluate strike bonus
         int StrikeBonus(int i)
         {
             return pinFall[i + 1] + pinFall[i + 2];
         }
 
+        //Method to evaluate spare bonus
         int SpareBonus(int i)
         {
             return pinFall[i + 2];
         }
 
+        //method to evaluate score
         public int GetScore()
         {
             int score = 0;
             int i = 0;
-            for (int frame=0;frame<10;frame++)
+            try
             {
-                if (IsStrike(i))
+                for (int frame = 0; frame < 10; frame++)
                 {
-                    score += 10 + StrikeBonus(i);
-                    i += 1;
-                }
-                else if (IsSpare(i))
-                {
-                    score +=10+ SpareBonus(i);
-                    i += 2;
-                }
+                    if (IsStrike(i))
+                    {
+                        score += 10 + StrikeBonus(i);
+                        i += 1;
+                    }
+                    else if (IsSpare(i))
+                    {
+                        score += 10 + SpareBonus(i);
+                        i += 2;
+                    }
 
-                else
-                {
-                    score += pinFall[i] + pinFall[i + 1];
-                    i += 1;
+                    else
+                    {
+                        score += pinFall[i] + pinFall[i + 1];
+                        i += 1;
+                    }
                 }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.GetType().Name  );
             }
             return score;   
         }
