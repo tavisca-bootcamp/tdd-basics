bb8dd933b066e31edbbe075452285f9a2d501d31
tavisca-ragupta <ragupta@tavisca.in>
2019-07-02 11:47:42 +0530

    First Test

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -6,45 +6,21 @@ namespace BowlingBall.Tests
     public class GameFixture
     {
 		private int _game = null;
-     
-       [Fact]
+        [Fact]
         public void DummyTest()
         {
             // This is a dummy test that will always pass.
         }
 		
-		
-		[Fact]
-		public void GutterBallTest()
+		 [Fact]
+	  	 public void GutterBallTest()
 		{
 		   _game = new Game(); 
 		    for(int i=0;i<20;i++)
-		       _game.Roll(0);
+		        _game.Roll(0);
 		    int score = _game.GetScore();
-		    int expectedValue = 0;
-		    Assert.Equal(expectedValue,score);
+		    int ExpectedValue = 0;
+		    Assert.Equal(ExpectedValue,score);
 		}
-		
-		
-		[Fact]
-		public void NoSpareNoStrikeTest()
-		{
-			_game = new Game();
-		 	 RollAutomatically(20,2);
-			 int expectedValue = 40;
-			 int score = _game.GetScore();
-			 Assert.Equal(expectedValue,score);
-		}
-		
-		
-		
-		
-		
-		private void RollAutomatically(int rollingCount,int pins)
-            {
-               for(int i=0;i<rollingCount;i++)
-	               _game.Roll(pins);
-            }
-    
-	}
+    }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -5,27 +5,10 @@ namespace BowlingBall
     public class Game
     {
 	   int score = 0;
-	   bool isSpare = false;
-	   int prevPin = 0;
         public void Roll(int pins)
         {
 			if(pins==0)
-			{		
-     		score += 0;
-			return;
-			}
-			score+=pins;
-				if(isSpare)
-			{
-				score+=pins;
-				isSpare = false;
-			}
-			if(pins+prevPin==10)
-			{
-			  isSpare = true;
-			}
-		
-			prevPin = pins;
+				score += 0
 			return;
             throw new NotImplementedException();
         }
