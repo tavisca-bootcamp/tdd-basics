9d15fc461a2f86c9f7529799c55c95b09da95b20
tavisca-ragupta <ragupta@tavisca.in>
2019-07-02 11:24:53 +0530

    First Test

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,22 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+		private int _game = null;
         [Fact]
         public void DummyTest()
         {
             // This is a dummy test that will always pass.
         }
+		
+		 [Fact]
+	  	 public void GutterBallTest()
+		{
+		   _game = new Game(); 
+		    for(int i=0;i<20;i++)
+		        _game.Roll(0);
+		    int score = _game.GetScore();
+		    int ExpectedValue = 0;
+		    Assert.Equal(ExpectedValue,score);
+		}
     }
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,13 +4,18 @@ namespace BowlingBall
 {
     public class Game
     {
+	   int score = 0;
         public void Roll(int pins)
         {
+			if(pins==0)
+				score += 0
+			return;
             throw new NotImplementedException();
         }
 
         public int GetScore()
         {
+			return score;
             throw new NotImplementedException();
         }
 
