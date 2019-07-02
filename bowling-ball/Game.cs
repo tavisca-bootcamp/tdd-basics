afd0104e4353f7917cb8fa05d08732937eba817d
tavisca-ragupta <ragupta@tavisca.in>
2019-07-02 11:52:27 +0530

    Secomd Test

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -5,10 +5,27 @@ namespace BowlingBall
     public class Game
     {
 	   int score = 0;
+	   bool isSpare = false;
+	   int prevPin = 0;
         public void Roll(int pins)
         {
 			if(pins==0)
-				score += 0
+			{		
+     		score += 0;
+			return;
+			}
+			score+=pins;
+				if(isSpare)
+			{
+				score+=pins;
+				isSpare = false;
+			}
+			if(pins+prevPin==10)
+			{
+			  isSpare = true;
+			}
+		
+			prevPin = pins;
 			return;
             throw new NotImplementedException();
         }
