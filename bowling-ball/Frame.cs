31080626e55e605d76f637376d52343d8156c918
vijay <vreddy@tavisca.in>
2019-07-02 15:18:31 +0530

    created two more classes and made more readable.

--- bowling-ball/Frame.cs
+++ bowling-ball/Frame.cs
@@ -4,22 +4,38 @@ public class Frame
 {
     public int FirstRoll=0;
     public int SecondRoll=0;
-    public int Score=0;
-    
-    public void CalculateScore()
+    public int Score = 0;
+
+    public void DoFirstRoll(int pins)
+    {
+        FirstRoll = pins;
+    }
+
+
+    public void DoSecondRoll(int pins)
+    {
+        SecondRoll = pins;
+    }
+    public void CalculateRollsScore()
     {
         Score = FirstRoll + SecondRoll;
     }
+
+    public void TenthFrameUpdate(int pins)
+    {
+        Score += pins;
+    }
+
     public bool IsSpare()
     {    if (FirstRoll == 10)
             return false;
         return FirstRoll + SecondRoll == 10;
     }
+
     public bool IsStrike()
     {
         return FirstRoll == 10;
     }
 
-
-	
+    
 }
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -3,105 +3,72 @@ using System.Collections.Generic;
 
 namespace BowlingBall
 {
-    public class Game
+    public class Game:FramesManage
     {
-        private Frame[] Frames;
-        private int OpportunityCount= 0;
-        private int Index = 0;
-        public const int  LASTFRAMEINDEX= 9;
-        public const int STRIKE = 10;
+       
+        private int OpportunityNumber= 1;
+        Frame CurrentFrame;
+        
 
-        public Game()
+        public Game():base(new Frame[10])
         {
-            Frames = new Frame[10];
+            
         }
+
         public void Roll(int pins)
         {
-             if(Index==LASTFRAMEINDEX+1)//special case for third roll in tenth frame
+            if (PresentFrame==FRAMESEXCEEDED)//it is for handling third roll of tenth frame;
             {
-                TenthFrameUpdate(pins);
+                CurrentFrame.TenthFrameUpdate(pins);
                 return;
+
             }
 
+                   switch (OpportunityNumber)
+                  {
 
-               
-           
-          if (pins == STRIKE && Index!=LASTFRAMEINDEX)
-            {
-                Frames[Index] = new Frame();
-                Frames[Index].FirstRoll = pins;
-                Frames[Index].CalculateScore();
-                UpdateFramesScore();
-                Index++;
-            }
-            else if(OpportunityCount==0)
-            {
-                Frames[Index] = new Frame();
-                Frames[Index].FirstRoll = pins;
-                OpportunityCount=1;
+                        case var opportunity when opportunity==FIRST:
+                                                CurrentFrame= new Frame();
+                                                CurrentFrame.DoFirstRoll(pins);
+                                                if (pins == STRIKEPINS&&PresentFrame!=LASTFRAMEINDEX)
+                                                     OpportunityNumber = COMPLETED;
+                                                 else
+                                                      OpportunityNumber++;
+                                break;
                 
-                
+                        case var opportunity when opportunity== SECOND:
+                                              CurrentFrame.DoSecondRoll(pins);
+                                              OpportunityNumber = COMPLETED;
 
-            }
-            else if(OpportunityCount==1)
-            {
-                Frames[Index].SecondRoll = pins;
-                Frames[Index].CalculateScore();
-                UpdateFramesScore();
-                OpportunityCount = 0;
-                Index++;
+                                break;
+                  }
 
-
-            }
+            UpdateCurrentFrame();
+            
                 
         }
+
 
         public int GetScore()
         {
-            return Frames[LASTFRAMEINDEX].Score;
+            return CurrentFrame.Score;
         }
+
+
+        private void UpdateCurrentFrame()
+        {
+            if (OpportunityNumber == COMPLETED)
+            {
+                UpdateInFrames(CurrentFrame);
+                OpportunityNumber = FIRST;
+            }
+        }
+
+        
         //UpdateFramesScore method is used to update previous frames if they are strike or  spare
-        public void UpdateFramesScore()
-        {    if (Index == 0)//for first frame we don't have previous previous frame to update score
-                return;
-            if (Index>1 &&Frames[Index-1].IsStrike()&&Frames[Index-2].IsStrike())
-            {   if (Frames[Index].IsStrike())
-                {
-                    Frames[Index - 2].Score += 10;
-                    Frames[Index - 1].Score += 20;
-                }
-                else
-                {
-                    Frames[Index - 2].Score += Frames[Index].FirstRoll;
-                    Frames[Index-1].Score+=(Frames[Index].FirstRoll+ Frames[Index].Score);
-                }
-            }
-            else if(Index>0 && Frames[Index-1].IsStrike())
-            {
-                Frames[Index - 1].Score += Frames[Index].Score;
-            }
-          else if(Index>0 && Frames[Index-1].IsSpare())
-            {
-                Frames[Index - 1].Score += Frames[Index].FirstRoll;
-            }
-
-            Frames[Index].Score += Frames[Index - 1].Score;
-
-        }
-        public void TenthFrameUpdate(int tenthFrameThirdRollPins)
-        {       if (Frames[LASTFRAMEINDEX-1].IsStrike())
-            {
-                Frames[LASTFRAMEINDEX-1].Score += Frames[LASTFRAMEINDEX].SecondRoll;
-                Frames[LASTFRAMEINDEX].Score += Frames[LASTFRAMEINDEX].SecondRoll;
-            }
-
-            Frames[LASTFRAMEINDEX].Score += tenthFrameThirdRollPins; 
-        }
-        //public void Traverse()
-        //{
-        //    for (int i = 0; i < 10; i++)
-        //        Console.WriteLine(Frames[i].Score);
-        //}
+       
+          
+       
            
         
 
