fc50f8d787a0b759a83e9fc0bbd897620bb4669b
tavisca-rgupta-2019 <rgupta@tavisca.in>
2019-07-03 17:20:45 +0530

    tdd basics 1 assignment

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -3,12 +3,64 @@ using Xunit;
 
 namespace BowlingBall.Tests
 {
+
     public class GameFixture
     {
+        Game obj = new Game();
         [Fact]
         public void DummyTest()
         {
             // This is a dummy test that will always pass.
         }
+        [Fact]
+        public void ForAllZeroes()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                obj.Roll(0);
+            }
+            
+            Assert.Equal(0, obj.GetScore());
+
+        }
+        [Fact]
+        public void ForAllOnes()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                obj.Roll(1);
+            }
+            Assert.Equal(20, obj.GetScore());
+        }
+        [Fact]
+        public void ForAllStrikes()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                obj.Roll(10);
+            }
+            Assert.Equal(300, obj.GetScore());
+        }
+        [Fact]
+        public void ForAllSpares()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                obj.Roll(6);
+                obj.Roll(4);
+            }
+            obj.Roll(5);
+            Assert.Equal(159, obj.GetScore());
+        }
+        [Fact]
+        public void ForStrikeAndSpareInEnd()
+        {
+            int[] pins_down = new int[] { 5, 2, 6, 4, 10, 7, 2, 9, 1, 4, 4, 10, 8, 2, 4, 5, 8, 2, 10 };
+            obj.Roll(pins_down);
+            Assert.Equal(140, obj.GetScore());
+        }
     }
 }
+
+
+
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,16 +4,56 @@ namespace BowlingBall
 {
     public class Game
     {
+        private int[] roll = new int[21];
+        private int CurrentRoll = 0;
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            roll[CurrentRoll++] = pins;
+
+            //throw new NotImplementedException();
+        }
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+            {
+                roll[i] = pins[i];
+            }
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int TotalScore = 0;
+            int i = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (roll[i] == 10 && i < 19)
+                {
+                    TotalScore += 10 + roll[i + 1] + roll[i + 2];
+                    i++;
+                }
+                else if ((roll[i] + roll[i + 1]) == 10)
+                {
+                    TotalScore += 10 + roll[i + 2];
+                    i = i + 2;
+                }
+                else
+                {
+                    TotalScore += roll[i] + roll[i + 1];
+                    i = i + 2;
+                }
+            }
+            return TotalScore;
+
+
+            //throw new NotImplementedException();
         }
 
     }
 }
+
+
+
+    
+
 
