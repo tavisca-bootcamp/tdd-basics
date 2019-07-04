32ffc6f60ec89773662d3b8719298101bf40b726
Dipika Mandal <dmandal@tavisca.in>
2019-07-04 12:22:48 +0530

    Added tests to test StrikeFrame class

---
+++ BowlingBall.Tests/StrikeFrameTest.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace BowlingBall.Tests
+{
+    public class StrikeFrameTest
+    {
+
+        [Fact]
+        public void NegativeRollsToCreateStrikeFrame()
+        {
+            //Negative roll shall throw exception
+            try
+            {
+                StrikeFrame strikeFrame=new StrikeFrame(-1, -2, -8);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void FirstRollNegativeToCreateStrikeFrame()
+        {
+            //Negative roll shall throw exception
+            try
+            {
+                StrikeFrame strikeFrame = new StrikeFrame(-1, 2, 8);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void SecondRollNegativeToCreateStrikeFrame()
+        {
+            //Negative roll shall throw exception
+            try
+            {
+                StrikeFrame strikeFrame = new StrikeFrame(10, -2, 8);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void ThirdRollNegativeToCreateStrikeFrame()
+        {
+            //Negative roll shall throw exception
+            try
+            {
+                StrikeFrame strikeFrame = new StrikeFrame(10, 2, -8);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void RollIsLessThanExpectedToCreateStrikeFrame()
+        {
+            //strike frame shall have a roll of 10 pins
+            try
+            {
+                StrikeFrame strikeFrame = new StrikeFrame(5, 4, 10);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void RollIsMoreThanExpectedToCreateStrikeFrame()
+        {
+            //strike frame shall have a roll of 10 pins
+            try
+            {
+                StrikeFrame strikeFrame = new StrikeFrame(15, 6, 10);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void InvalidFirstRollToCreateStrikeFrame()
+        {
+            //roll can't be more than 10
+            try
+            {
+                StrikeFrame strikeFrame = new StrikeFrame(50, 4, 1);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void InvalidSecondRollToCreateStrikeFrame()
+        {
+            //roll can't be more than 10
+            try
+            {
+                StrikeFrame strikeFrame = new StrikeFrame(10, 14, 1);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void InvalidThirdRollToCreateStrikeFrame()
+        {
+            //roll can't be more than 10
+            try
+            {
+                StrikeFrame strikeFrame = new StrikeFrame(10, 4, 11);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void ValidSetOfRollsToCreateStrikeFrame()
+        {
+            try
+            {
+                StrikeFrame strikeFrame = new StrikeFrame(10, 5, 1);
+            }
+            catch (Exception e)
+            {
+                //No exception is expected
+                Assert.True(false);
+            }
+            Assert.True(true);
+        }
+    }
+}
