2b4c98815df53d0f25127c00f902eb9e9e823e7a
Dipika Mandal <dmandal@tavisca.in>
2019-07-04 17:44:11 +0530

    Implemented tests to check how exceptions are handled by TheBowlingGame class using rollslist

---
+++ BowlingBall.Tests/TheBowlingGameTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using Xunit;
+using BowlingBall;
+namespace BowlingBall.Tests
+{
+    public class TheBowlingGameTest
+    {
+        [Fact]
+        public void ScoreCannotBeMoreThan300()
+        {
+            ArrayList rolls = new ArrayList();
+            for(int i = 0; i < 30; i++)
+            {
+                rolls.Add(10);
+            }
+            try
+            {
+                TheBowlingGame theBowlingGame = new TheBowlingGame(rolls);
+            }
+            catch(Exception e)
+            {
+                //throws exception in creating rolls scoring more than 300
+                return;
+            }
+            Assert.True(false);            
+        }
+        [Fact]
+        public void BowlingGameWithLessRolls()
+        {
+            ArrayList rolls = new ArrayList();
+            for (int i = 0; i < 6; i++)
+                rolls.Add(10);
+            try
+            {
+                TheBowlingGame theBowlingGame = new TheBowlingGame(rolls);
+            }
+            catch(Exception e)
+            {
+                //throws exception in creating less rolls
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void BowlingGameWithMoreRolls()
+        {
+            ArrayList rolls = new ArrayList();
+            for (int i = 0; i < 22; i++)
+                rolls.Add(5);
+            try
+            {
+                TheBowlingGame theBowlingGame = new TheBowlingGame(rolls);
+            }
+            catch (Exception e)
+            {
+                //throws exception in creating more rolls
+                return;
+            }
+            Assert.True(false);
+        }
+    }
+}
