7109f09f590181c8b733ce96165a404007749133
tavisca-nnarkhede <51942815+tavisca-nnarkhede@users.noreply.github.com>
2019-07-04 12:02:03 +0530

    Update Game.cs

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,19 +1,55 @@
-﻿using System;
-
+using System;
+using System.Collections.Generic;
 namespace BowlingBall
 {
     public class Game
     {
-        public void Roll(int pins)
+        List<int> rolls;
+        public Game()
         {
-            throw new NotImplementedException();
+            rolls = new List<int>();
         }
-
+        public void Roll(int pinsKnocked)
+        {
+            rolls.Add(pinsKnocked);
+        }
         public int GetScore()
         {
-            throw new NotImplementedException();
+            var bowlingScore = 0;
+            var currentRoll = 0;
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(currentRoll))
+                {
+                    bowlingScore += rolls[currentRoll] + rolls[currentRoll + 1] + rolls[currentRoll + 2];
+                    currentRoll++;
+                }
+                else if (IsSpare(currentRoll))
+                {
+                    bowlingScore += 10 + rolls[currentRoll + 2];
+                    currentRoll += 2;
+                }
+                else
+                {
+                    bowlingScore += rolls[currentRoll] + rolls[currentRoll + 1];
+                    currentRoll += 2;
+                }
+            }
+            return bowlingScore;
         }
-
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
+                rolls.Add(pins[i]);
+        }
+        private bool IsSpare(int roll)
+        {
+            return rolls[roll] + rolls[roll + 1] == 10;
+        }
+        private bool IsStrike(int roll)
+        {
+            return rolls[roll] == 10;
+        }
     }
 }
 
