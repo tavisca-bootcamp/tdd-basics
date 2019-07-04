149a6606bdbf446f6c789ab0c4ad39da51d9dfb6
tavisca-arupade <51902733+tavisca-arupade@users.noreply.github.com>
2019-07-04 16:56:44 +0530

    Updated GameTest class

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -1,14 +1,86 @@
 using System;
 using Xunit;
+using BowlingBall;
+using Moq;
 
 namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+
         [Fact]
-        public void DummyTest()
+        public void Game_when_UserRollsValidInput()
         {
-            // This is a dummy test that will always pass.
+            Game game = new Game();
+            int[] pins = { 1, 2, 3, 4 };
+
+            game.Roll(pins);
+
+            Assert.Equal(10, game.GetScore());
+
         }
+
+        [Fact]
+        public void Game_when_UserRollsAllZeroes()
+        {
+            Game game = new Game();
+            int[] pins = { 0,0,0,0,0,0,0,0 };
+
+            game.Roll(pins);
+
+            Assert.Equal(0, game.GetScore());
+        }
+
+        [Fact]
+        public void Game_when_UserRollsAllOnes()
+        {
+            Game game = new Game();
+            int[] pins = { 1, 1, 1, 1, 1, 1, 1, 1 };
+
+            game.Roll(pins);
+
+            Assert.Equal(8, game.GetScore());
+        }
+
+        [Fact]
+        public void Game_when_UserRollsSpare()
+        {
+            Game game = new Game();
+            int[] pins = { 9, 1 };
+            game.Roll(pins);
+
+            Mock<IFrames> mockObject = new Mock<IFrames>();
+            mockObject.Setup(m => m.IsSpare()).Returns(true);
+        }
+
+        [Fact]
+
+        //Failed because odd length input, Frames class sets an array of 2
+        public void Game_when_UserRollsStrike()
+        {
+            Game game = new Game();
+            int[] pins = { 10 , 0 };
+            game.Roll(pins);
+
+            Mock<IFrames> mockObject = new Mock<IFrames>();
+            mockObject.Setup(m => m.IsStrike()).Returns(true);
+        }
+
+        [Fact]
+        public void Game_when_UserRollsOddInput()
+        {
+            Game game = new Game();
+            int[] pins = { 1, 2, 3 };
+
+            game.Roll(pins);
+
+            // 1+2 = 3 beacuse the user is still playing for second frame (3,-)
+            Assert.Equal(3, game.GetScore());
+        }
+
+
+
+
+
     }
 }
