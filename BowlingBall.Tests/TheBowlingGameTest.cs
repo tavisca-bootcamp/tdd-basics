1ea7857010e47fed9239a776f9cc185a6d3ec6bf
Dipika Mandal <dmandal@tavisca.in>
2019-07-04 18:10:08 +0530

    Added tests for the main function in TheBowlingGame Class

--- BowlingBall.Tests/TheBowlingGameTest.cs
+++ BowlingBall.Tests/TheBowlingGameTest.cs
@@ -61,5 +61,90 @@ namespace BowlingBall.Tests
             }
             Assert.True(false);
         }
+        [Fact]
+        public void PerfectGameScoreTest()
+        {
+            ArrayList rolls = new ArrayList();
+            int expectedValue = 300;
+            int actualValue;
+
+            for (int i = 0; i < 12; i++)
+                rolls.Add(10);
+            TheBowlingGame theBowlingGame = new TheBowlingGame(rolls);
+            actualValue = theBowlingGame.GetScore();
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+        [Fact]
+        public void AllSpareTest()
+        {
+            ArrayList rolls = new ArrayList();
+            int expectedValue = 150;
+            int actualValue;
+
+            for (int i = 0; i < 21; i++)
+                rolls.Add(5);
+            TheBowlingGame theBowlingGame = new TheBowlingGame(rolls);
+            actualValue = theBowlingGame.GetScore();
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+        [Fact]
+        public void AllValues2Test()
+        {
+            ArrayList rolls = new ArrayList();
+            int expectedValue = 40;
+            int actualValue;
+
+            for (int i = 0; i < 20; i++)
+                rolls.Add(2);
+            TheBowlingGame theBowlingGame = new TheBowlingGame(rolls);
+            actualValue = theBowlingGame.GetScore();
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+        [Fact]
+        public void AllValues3Test()
+        {
+            ArrayList rolls = new ArrayList();
+            int expectedValue = 60;
+            int actualValue;
+
+            for (int i = 0; i < 20; i++)
+                rolls.Add(3);
+            TheBowlingGame theBowlingGame = new TheBowlingGame(rolls);
+            actualValue = theBowlingGame.GetScore();
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+        [Fact]
+        public void ExampleTest()
+        {
+            ArrayList rolls = new ArrayList();
+            int expectedValue = 187;
+            int actualValue;
+
+            rolls.Add(10);
+            rolls.Add(9);
+            rolls.Add(1);
+            rolls.Add(5);
+            rolls.Add(5);
+            rolls.Add(7);
+            rolls.Add(2);
+            rolls.Add(10);
+            rolls.Add(10);
+            rolls.Add(10);
+            rolls.Add(9);
+            rolls.Add(0);
+            rolls.Add(8);
+            rolls.Add(2);
+            rolls.Add(9);
+            rolls.Add(1);
+            rolls.Add(10);
+            TheBowlingGame theBowlingGame = new TheBowlingGame(rolls);
+            actualValue = theBowlingGame.GetScore();
+
+            Assert.Equal(expectedValue, actualValue);
+        }
     }
 }
