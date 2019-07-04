daa719b2c40caf9002f8c71aad9eb93fe0c8e3d5
Dipika Mandal <dmandal@tavisca.in>
2019-07-04 09:07:40 +0530

    Created Frame class for storing data of each frame

---
+++ bowling-ball/Frame.cs
@@ -0,0 +1,14 @@
+using System;
+namespace BowlingBall
+{
+    class Frame
+    {
+        public var RollList;
+        public var NumberOfRolls;
+        public var Score;
+        public int GetScore()
+        {
+            retrun Score;
+        }
+    }
+}
