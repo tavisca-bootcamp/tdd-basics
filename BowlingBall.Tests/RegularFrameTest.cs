c3ce58ac2578f5f0e84dd791373505a2cd925df7
Dipika Mandal <dmandal@tavisca.in>
2019-07-04 11:13:10 +0530

    Tests to ckeck is invalid rolls throws exception

---
+++ BowlingBall.Tests/RegularFrameTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BowlingBall;
+using Xunit;
+namespace BowlingBall.Tests
+{
+    public class RegularFrameTest
+    {
+        [Fact]
+        public void NegativeRollsToCreateRegularFrame()
+        {
+            //Negative roll shall throw exception
+            try
+            {
+                RegularFrame regularFrame = new RegularFrame(-1, -2);
+            }
+            catch(Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void OneNegativeRollToCreateRegularFrame()
+        {
+            //Negative roll shall throw exception
+            try
+            {
+                RegularFrame regularFrame = new RegularFrame(-1, 2);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void InvalidRollToCreateRegularFrame()
+        {
+            //Negative roll shall throw exception
+            try
+            {
+                RegularFrame regularFrame = new RegularFrame(100,1);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void RollsSumUpToValueMoreThanExpectedToCreateRegularFrame()
+        {
+            //Negative roll shall throw exception
+            try
+            {
+                RegularFrame regularFrame = new RegularFrame(5, 6);
+            }
+            catch (Exception e)
+            {
+                //Throws exception
+                return;
+            }
+            Assert.True(false);
+        }
+
+    }
+}
