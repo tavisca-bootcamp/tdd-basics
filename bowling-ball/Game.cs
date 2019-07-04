8f766e405428ff77ce460fdbd1ef3235490b3eee
tavisca-arupade <51902733+tavisca-arupade@users.noreply.github.com>
2019-07-04 16:53:40 +0530

    Updated Game class

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,19 +1,50 @@
-﻿using System;
+using System;
 
 namespace BowlingBall
 {
     public class Game
     {
-        public void Roll(int pins)
+        Frames [] frames = new Frames[10];
+        int PointerToFrame;
+        const int TotalPossibleFrames = 10;
+        int ActualFrames;
+        public Game()
         {
-            throw new NotImplementedException();
+            PointerToFrame = 0;
+
+            for (int index = 0; index < TotalPossibleFrames; index++)
+                frames[index] = new Frames();
+        }
+        public void Roll(int [] pins)
+        {
+            
+            int ptr = 0;
+           while(ptr < pins.Length)
+            {
+              
+                    frames[PointerToFrame].SetFrame(pins[ptr++], 0);
+                    frames[PointerToFrame].SetFrame(pins[ptr++], 1);
+              
+                PointerToFrame++;
+            }
+            ActualFrames = PointerToFrame;
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int TotalScores = 0;
+            for(int ptr=0; ptr <= ActualFrames; ptr++)
+            {
+                if (frames[ptr].IsSpare() == true)
+                    TotalScores += 10 + frames[ptr + 1].SpareBonus();
+                else if (frames[ptr].IsStrike() == true)
+                    TotalScores += 10 + frames[ptr + 1].StrikeBonus();
+                else if (frames[ptr].IsSpare() == false && frames[ptr].IsStrike() == false )
+                    TotalScores += frames[ptr].CalculateScore();
+                
+            }
+            return TotalScores;
         }
-
     }
 }
 
