da53a6d8b5c2dcfd54bc20dbb17595aa6761daac
tavisca-smulay <smulay@tavisca.in>
2019-07-05 00:05:28 +0530

    Solution Refractor

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -1,92 +1,82 @@
 using System;
 using Xunit;
-
-namespace BowlingBall.Tests
-{
-    public class GameFixtureusing System;
-using Xunit;
 using System.Collections.Generic;
 
 namespace BowlingBall.Tests
-{
-    public class GameFixture
     {
-        #region PRIVATE METHODS
-        private Game createObjectOfGame(){
-        return new Game();
-}
-        private void RollPins(Game game, int noOfRolls, int noOfPinsPerRoll)
+        public class GameFixture
         {
-            //throw new NotImplementedException();
-            for (var i = 0; i < noOfRolls; i++)
+            [Fact]
+            public void AllZeroesTest()
             {
-                game.Roll(noOfPinsPerRoll);
+                Game game = createObjectOfGame();
+                RollPins(game, 20, 0);
+                Assert.Equal(0, game.GetScore());
             }
-        }
 
-        private void RollSpare(Game game) {
-            game.Roll(5);
-            game.Roll(5);
-        }
+            [Fact]
+            public void AllOnesTest()
+            {
+                Game game = createObjectOfGame();
+                RollPins(game, 20, 1);
+                Assert.Equal(20, game.GetScore());
+            }
 
-        private void RollStrike(Game game) {
-            game.Roll(10);
-        }
+            [Fact]
+            public void OneSpareTest()
+            {
+                Game game = createObjectOfGame();
+                RollSpare(game);
+                game.Roll(3);
+                RollPins(game, 17, 0);
+                Assert.Equal(16, game.GetScore());
+            }
 
-        #endregion
-        [Fact]
-        public void AllZeroesTest()
-        {
-            Game game = createObjectOfGame();
-            RollPins(game, 20, 0);
-            Assert.Equal(0, game.GetScore());
-        }
+            [Fact]
+            public void OneStrikeTest()
+            {
+                Game game = createObjectOfGame();
+                RollStrike(game);
+                game.Roll(3);
+                game.Roll(4);
+                RollPins(game, 16, 0);
+                Assert.Equal(24, game.GetScore());
+            }
 
-        [Fact]
-        public void AllOnesTest()
-        {
-            Game game = createObjectOfGame();
-            RollPins(game, 20, 1);
-            Assert.Equal(20, game.GetScore());
-        }
+            [Fact]
+            public void PerfectGameTest()
+            {
+                Game game = createObjectOfGame();
+                RollPins(game, 12, 10);
+                Assert.Equal(300, game.GetScore());
+            }
 
-        [Fact]
-        public void OneSpareTest()
-        {
-            Game game = createObjectOfGame();
-            RollSpare(game);
-            game.Roll(3);
-            RollPins(game, 17, 0);
-            Assert.Equal(16, game.GetScore());
-        }
+            #region PRIVATE METHODS
+            private Game createObjectOfGame()
+            {
+                return new Game();
+            }
+            private void RollPins(Game game, int noOfRolls, int noOfPinsPerRoll)
+            {
+                //throw new NotImplementedException();
+                for (var i = 0; i < noOfRolls; i++)
+                {
+                    game.Roll(noOfPinsPerRoll);
+                }
+            }
 
-        [Fact]
-        public void OneStrikeTest()
-        {
-            Game game = createObjectOfGame(); 
-            RollStrike(game);
-            game.Roll(3);
-            game.Roll(4);
-            RollPins(game, 16, 0);
-            Assert.Equal(24, game.GetScore());
-        }
+            private void RollSpare(Game game)
+            {
+                game.Roll(5);
+                game.Roll(5);
+            }
 
-        [Fact]
-        public void PerfectGameTest()
-        {
-            Game game = createObjectOfGame();
-            RollPins(game, 12, 10);
-            Assert.Equal(300, game.GetScore());
-        }
+            private void RollStrike(Game game)
+            {
+                game.Roll(10);
+            }
 
+            #endregion
+
+        }
     }
-}
-
-    {
-        [Fact]
-        public void DummyTest()
-        {
-            // This is a dummy test that will always pass.
-        }
-    }
-}
