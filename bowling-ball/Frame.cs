742283f601d601d4ce58e110abf642860c3c34eb
Dipika Mandal <dmandal@tavisca.in>
2019-07-04 11:10:51 +0530

    Added code to check validity of a roll

--- bowling-ball/Frame.cs
+++ bowling-ball/Frame.cs
@@ -11,5 +11,11 @@ namespace BowlingBall
         {
             return Score;
         }
+        public static bool IsValidRoll(int roll)
+        {
+            if ((roll <= 10) && (roll >= 0))
+                return true;
+            return false;
+        }
     }
 }
