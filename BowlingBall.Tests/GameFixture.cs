0568fc3ede249932e2c1a06bf38d5c0ab3b00d10
tavisca-arupade <51902733+tavisca-arupade@users.noreply.github.com>
2019-07-05 18:43:41 +0530

    Updated GameTestClass - v4

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -9,7 +9,7 @@ namespace BowlingBall.Tests
     {
 
         [Fact]
-        public void Game_when_UserRollsValidInput()
+        public void Game_when_UserRollsValidInput_should_ReturnCorrectScore()
         {
             Game game = new Game();
             int[] pins = { 1, 2, 3, 4 };
@@ -21,7 +21,7 @@ namespace BowlingBall.Tests
         }
 
         [Fact]
-        public void Game_when_UserRollsAllZeroes()
+        public void Game_when_UserRollsAllZeroes_should_ReturnCorrectScore()
         {
             Game game = new Game();
             int[] pins = { 0,0,0,0,0,0,0,0 };
@@ -32,7 +32,7 @@ namespace BowlingBall.Tests
         }
 
         [Fact]
-        public void Game_when_UserRollsAllOnes()
+        public void Game_when_UserRollsAllOnes_should_ReturnCorrectScore()
         {
             Game game = new Game();
             int[] pins = { 1, 1, 1, 1, 1, 1, 1, 1 };
@@ -43,7 +43,7 @@ namespace BowlingBall.Tests
         }
 
         [Fact]
-        public void Game_when_UserRollsSpare()
+        public void Game_when_UserRollsSpare_should_ReturnCorrectScore()
         {
             Game game = new Game();
             int[] pins = { 9, 1 , 1 , 2};
@@ -57,7 +57,7 @@ namespace BowlingBall.Tests
         [Fact]
 
         //Failed because odd length input, Frames class sets an array of 2
-        public void Game_when_UserRollsStrike()
+        public void Game_when_UserRollsStrike_should_ReturnCorrectScore()
         {
             Game game = new Game();
             int[] pins = { 10  , 2 , 3};
@@ -69,7 +69,19 @@ namespace BowlingBall.Tests
         }
 
         [Fact]
-        public void Game_when_UserRollsOddInput()
+
+        public void Game_when_UserRollsForAllValues_should_ReturnCorrectScore()
+        {
+            Game game = new Game();
+            int[] pins = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+            game.Roll(pins);
+
+            Assert.Equal(90 , game.GetScore());
+        }
+
+        [Fact]
+        public void Game_when_UserRollsOddInput_should_ReturnCorrectScore()
         {
             Game game = new Game();
             int[] pins = { 1, 2, 3 };
@@ -79,5 +91,53 @@ namespace BowlingBall.Tests
             // 1+2 = 3 beacuse the user is still playing for second frame (3,-)
             Assert.Equal(3, game.GetScore());
         }
+
+        [Fact]
+        public void Game_when_UserRollsInValidInput_should_ThrowException()
+        {
+            Game game = new Game();
+            int[] pins = { 1, 2, 45, 8 };
+
+            Exception ex = Assert.Throws<ArgumentOutOfRangeException>(() => game.Roll(pins));
+
+            Assert.Equal("Specified argument was out of the range of valid values.", ex.Message);
+        }
+
+        [Fact ]
+
+        public void Game_when_UserRollsMoreThanPossiblePins_should_ThrowException()
+        {
+            Game game = new Game();
+            int[] pins = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2 };
+
+            Exception ex = Assert.Throws<ArgumentOutOfRangeException>(() => game.Roll(pins));
+
+            Assert.Equal("Specified argument was out of the range of valid values.", ex.Message);
+        }
+
+        [Fact]
+
+        public void Game_when_UserRollsSpareOnLastFrame_should_ReturnCorrectScore()
+        {
+            Game game = new Game();
+            int[] pins = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 1 };
+
+            game.Roll(pins);
+
+            Assert.Equal(92, game.GetScore());
+        }
+
+        [Fact]
+
+        public void Game_when_UserRollsStrikeOnLastFrame_should_ReturnCorrectScore()
+        {
+            Game game = new Game();
+            int[] pins = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 1 };
+
+            game.Roll(pins);
+
+            Assert.Equal(92, game.GetScore());
+        }
+
     }
 }
