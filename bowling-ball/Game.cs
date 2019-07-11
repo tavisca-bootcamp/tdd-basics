5dd483e4f1c9fb84749a55aad402202b04530bf1
Sheth Dhvani <dsheth@tavisca.in>
2019-07-11 10:16:13 +0530

    Implemented functions for the game

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -4,14 +4,53 @@ namespace BowlingBall
 {
     public class Game
     {
+        private int score = 0;
+        private int[] rolls;
+        private int currentRoll = 0;
+
+        public Game()
+        {
+            rolls = new int[21];
+        }
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            score += pins;
+            rolls[currentRoll++] = pins; 
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int frameIndex = 0;
+            for(int frame=0; frame<10; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+            return score;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return (rolls[frameIndex] + rolls[frameIndex + 1] == 10);
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return (rolls[frameIndex] == 10);
         }
 
     }
