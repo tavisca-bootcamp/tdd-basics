a526ec2f32ef8b65b330a634b979f7787013d96b
Chandan-97 <cp564738@gmail.com>
2019-07-11 10:20:25 +0530

    Working Code

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,28 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+        private Game game = new Game();
+
+        public GameFixture(){
+            game = new Game();
+        }
+
         [Fact]
         public void DummyTest()
         {
             // This is a dummy test that will always pass.
         }
+
+        [Fact]
+        public void BasicTest1()
+        {
+            int[] scorelist = {10, 9, 1, 5, 5, 7, 2, 10, 10, 10, 9, 0, 8, 2, 9, 1, 10};
+            for(var i=0; i<scorelist.Length; i++){
+                game.Roll(scorelist[i]);
+            }
+
+            Assert.Equal(187, game.GetScore());
+        }
     }
 }
+
--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,19 +1,69 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace BowlingBall
 {
     public class Game
     {
+        private List <int> rolls;
+        private int rollno=0;
+        private const int MAXROLLs = 21;
+        private const int MAXFRAMES = 10;
+
+        public Game(){
+            rolls = new List<int>();
+        }
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            if(rollno < MAXROLLs){
+                rolls.Add(pins);
+                rollno++;
+            }
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int current=0;
+            for(var frames=1; frames <= MAXFRAMES; frames++){
+                if(IsStrike(current)){
+                    score += StrikeScore(current);
+                    current++;
+                }
+                else if(IsSpare(current)){
+                    score += SpareScore(current);
+                    current+=2;
+                }
+                else{   
+                    score += Score(current++);
+                    score += Score(current++);
+                }
+            }
+
+            return score;
         }
 
+        private bool IsStrike(int current){
+            return rolls[current]==10;
+        }
+
+        private bool IsSpare(int current){
+            return rolls[current] + rolls[current+1] == 10;
+        }
+
+        private int StrikeScore(int current){
+            return rolls[current] + rolls[current+1] + rolls[current+2];
+        }
+
+        private int SpareScore(int current){
+            return StrikeScore(current);
+        }
+
+        private int Score(int current){
+            return rolls[current];
+        }
     }
 }
+
 
