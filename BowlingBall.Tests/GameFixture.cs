ae49789e770bd8e7837f7e8d3cd4d28c9800dd91
Sheth Dhvani <dsheth@tavisca.in>
2019-07-11 10:15:24 +0530

    Added tests

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,10 +5,62 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
-        [Fact]
-        public void DummyTest()
+        private Game game;
+
+        public GameFixture()
         {
-            // This is a dummy test that will always pass.
+            game = new Game();
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (int i = 0; i < n; i++)
+                game.Roll(pins);
+        }
+
+        [Fact]
+        public void TestGutterGame()
+        {
+            int n = 20;
+            int pins = 0;
+            RollMany(n, pins);
+            Assert.Equal(0, game.GetScore());
+        }
+
+        [Fact]
+        public void TestOnes()
+        {
+            int n = 20;
+            int pins = 1;
+            RollMany(n, pins);
+            Assert.Equal(20, game.GetScore());
+        }
+
+        [Fact]
+        public void TestSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            RollMany(17, 1);
+            Assert.Equal(37, game.GetScore());
+        }
+
+        [Fact]
+        public void TestStroke()
+        {
+            game.Roll(10);
+            game.Roll(4);
+            game.Roll(2);
+            RollMany(17, 0);
+            Assert.Equal(22, game.GetScore());
+        }
+
+        [Fact]
+        public void TestPerfectGame()
+        {
+            RollMany(12, 10);
+            Assert.Equal(300, game.GetScore());
         }
     }
 }
