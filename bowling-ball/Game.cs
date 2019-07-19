83959ac81f20f977783a760cefdffbc5c2d14931
Ojas <opahwa@tavisca.in>
2019-07-19 23:51:12 +0530

    Changes made

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,68 +1,47 @@
-﻿using System;
+﻿
+using System;
 
 namespace BowlingBall
 {
     public class Game
     {
-        public const int maximumBalls = 21;
-        private int[] rolls = new int[maximumBalls];
-        int Rollindex = 0;
-        int Ballindex = 0;
+
+        Frame frame = new Frame();
 
         public void Roll(int pins)
         {
-            rolls[Rollindex++] = pins;
+            frame.rolls[frame.Rollindex++] = pins;
         }
         public int GetScore()
         {
             int score = 0;
             for (int i = 0; i < 10; i++)
             {
-                if (isSpare(Ballindex))
+                if (frame.IsSpare(frame.Ballindex))
                 {
-                    score += scoreSpare(Ballindex);
+                    score += frame.ScoreSpare(frame.Ballindex);
                 }
-                else if (isStrike(Ballindex))
+                else if (frame.IsStrike(frame.Ballindex))
                 {
-                    score += scoreStrike(Ballindex);
+                    score += frame.ScoreStrike(frame.Ballindex);
                 }
                 else
                 {
-                    score += Score(Ballindex);
-                    Ballindex += 2;
+                    score += frame.Score(frame.Ballindex);
+                    frame.Ballindex += 2;
                 }
             }
-            if (Ballindex != Rollindex)
+            if (frame.Ballindex != frame.Rollindex)
             {
-                if (isStrike(Ballindex - 1) || isSpare(Ballindex - 2))
+                if (frame.IsStrike(frame.Ballindex - 1) || frame.IsSpare(frame.Ballindex - 2))
                 { }
                 else
                     return -1;
             }
-            Console.WriteLine(Ballindex + " " + Rollindex);
+
             return score;
         }
-        private int Score(int i)
-        {
-            return rolls[i] + rolls[i + 1];
-        }
-        private int scoreSpare(int i)
-        {
-            Ballindex = Ballindex + 2;
-            return 10 + rolls[i + 2];
-        }
-        private int scoreStrike(int i)
-        {
-            Ballindex++;
-            return 10 + rolls[i + 1] + rolls[i + 2];
-        }
-        private bool isSpare(int i)
-        {
-            return rolls[i] + rolls[i + 1] == 10;
-        }
-        private bool isStrike(int i)
-        {
-            return rolls[i] == 10;
-        }
+
     }
 }
+
