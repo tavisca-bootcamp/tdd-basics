2a287dd01d68bc4e27f97f9ce7d2408da7d665fe
tavisca-averma <51946115+tavisca-averma@users.noreply.github.com>
2019-07-20 21:24:33 +0530

    Update Game.cs

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,19 +1,68 @@
-﻿using System;
+using System;
 
 namespace BowlingBall
 {
     public class Game
     {
+        private const int MAX_ROLLS = 21;
+        private const int MAX_Frames = 10;
+        private int[] rolls = new int[MAX_ROLLS];
+        private int currentRoll = 0;
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            rolls[currentRoll++] = pins;
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < MAX_Frames; frame++)
+            {
+                if (IsStrike(frameIndex))
+                {
+                    score += 10 + StrikeBonus(frameIndex);
+                    frameIndex++;
+                }
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + SpareBonus(frameIndex);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += SumOfBallsInFrame(frameIndex);
+                    frameIndex += 2;
+                }
+            }
+            return score;
         }
 
+        private Boolean IsStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        private int SumOfBallsInFrame(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1];
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 2];
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+
+        private Boolean IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
     }
 }
 
