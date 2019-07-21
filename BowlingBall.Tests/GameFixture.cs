af8101203a0022f41cfac5d645dbb3e54cdab17c
tavisca-apriyadarshi <apriyadarshi@tavisca.in>
2019-07-21 18:31:39 +0530

    Q1 for Submission of A3 21/7/2019

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -5,17 +5,123 @@ namespace BowlingBall.Tests
 {
     public class GameFixture
     {
+        private Game bowlingGame;
         [Fact]
-        public void DummyTest()
+        public void ScoreCannotBeMoreThan300()
+        {
+            bowlingGame = new Game();
+            try
+            {
+                for(int i = 0; i < 15; i++)
+                {
+                    bowlingGame.Roll(10);
+                }
+            }
+            catch(Exception e)
+            {
+                //throws exception in creating rolls scoring more than 300
+                return;
+            }
+            Assert.True(false);            
+        }
+        [Fact]
+        public void BowlingGameWithLessRolls()
+        {
+            bowlingGame = new Game();
+            try
+            {
+                for(int i = 0; i < 6; i++)
+                {
+                    bowlingGame.Roll(10);
+                }
+            }
+            catch(Exception e)
+            {
+                //throws exception in creating less rolls
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void BowlingGameWithMoreRolls()
+        {
+            bowlingGame = new Game();
+            try
+            {
+                for(int i = 0; i < 22; i++)
+                {
+                    bowlingGame.Roll(5);
+                }
+            }
+            catch (Exception e)
+            {
+                //throws exception in creating more rolls
+                return;
+            }
+            Assert.True(false);
+        }
+        [Fact]
+        public void PerfectGameScoreTest()
+        {
+            var expectedValue = 300;
+            var actualValue=0;
+
+            bowlingGame = new Game();
+            for (int i = 0; i < 12; i++)
+                bowlingGame.Roll(10);
+            actualValue = bowlingGame.GetScore();
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+        [Fact]
+        public void AllSpareTest()
+        {
+            var expectedValue = 150;
+            var actualValue=0;
+
+            bowlingGame = new Game();
+            for (int i = 0; i < 21; i++)
+                bowlingGame.Roll(5);
+            actualValue = bowlingGame.GetScore();
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+        [Fact]
+        public void AllValues2Test()
+        {
+            var expectedValue = 40;
+            var actualValue=0;
+
+            bowlingGame = new Game();
+            for (int i = 0; i < 20; i++)
+                bowlingGame.Roll(2);
+            actualValue = bowlingGame.GetScore();
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+        [Fact]
+        public void AllValues3Test()
+        {
+            var expectedValue = 60;
+            var actualValue=0;
+
+            bowlingGame = new Game();
+            for (int i = 0; i < 20; i++)
+                bowlingGame.Roll(3);
+            actualValue = bowlingGame.GetScore();
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+        [Fact]
+        public void ExampleTest()
         {
             //arrange
             var game = new Game();
 
             game.Roll(10);
-
             game.Roll(9);
-            game.Roll(1);
 
+            game.Roll(1);
             game.Roll(5);
 
             game.Roll(5);
@@ -25,8 +131,8 @@ namespace BowlingBall.Tests
             game.Roll(10);
 
             game.Roll(10);
-
             game.Roll(10);
+
             game.Roll(9);
             game.Roll(0);
 
@@ -46,4 +152,7 @@ namespace BowlingBall.Tests
             Assert.Equal(expected, actual);
         }
     }
+
 }
+
+
