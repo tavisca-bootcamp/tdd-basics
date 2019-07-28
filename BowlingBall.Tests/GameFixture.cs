6e97d3b686b2b0ef65757b483db64cb3e6a26359
Singh <csingh@internal.tavisca.com>
2019-07-28 10:07:27 +0530

    Made necessary changes in assignment

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BowlingBall.Tests
@@ -6,9 +7,51 @@ namespace BowlingBall.Tests
     public class GameFixture
     {
         [Fact]
-        public void DummyTest()
+        public void Score_when_NeitherSpareNorStrike_ocurs_()
         {
-            // This is a dummy test that will always pass.
+            //Given
+            List<int> Frame = new List<int>() { 7, 2 };
+            var _Score = new Score(Frame);
+            //when
+            var TotalScore = _Score.GetTotalScore(7);
+            //then
+            Assert.Equal(9, TotalScore);
         }
+
+        [Fact]
+        public void Score_when_Spare_ocurs_()
+        {
+            //Given
+            List<int> Frame = new List<int>() { 9, 1, 5 };
+            var _Score = new Score(Frame);
+            //when
+            var TotalScore = _Score.GetTotalScore(9);
+            //then
+            Assert.Equal(15, TotalScore);
+        }
+        [Fact]
+        public void Score_when_RepeatedStrike_ocurs()
+        {
+            //Given
+            List<int> Frame = new List<int>() { 10, 10,10};
+            var _Score = new Score(Frame);
+            //when
+            var TotalScore = _Score.GetTotalScore(10);
+            //then
+            Assert.Equal(30, TotalScore);
+        }
+
+        [Fact]
+        public void Score_when_Strike_ocurs()
+        {
+            //Given
+            List<int> Frame = new List<int>() {10,9,1,5,5};
+            var _Score = new Score(Frame);
+            //when
+            var TotalScore=_Score.GetTotalScore(10);
+            //then
+            Assert.Equal(20,TotalScore);
+        }
+
     }
 }
---
+++ bowling-ball/Score.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingBall
+{
+    public class Score
+    {
+       
+        
+        int Total = 0;
+        private List<int> Frame;
+
+        public Score(List<int> Frame)
+        {
+            this.Frame = Frame;
+        }
+
+        public int GetTotalScore(int PinsFalled)
+        {
+            for(int i=Frame.IndexOf(PinsFalled);i<i+1;i++)
+            {
+                //for strike
+                //..........
+                if(Frame[i]==10 &&Frame[i+1]==10 && Frame[i+2]==10)
+                {
+                    Total += 30;
+                    break;
+                }
+                else if(Frame[i] == 10 && Frame[i + 1] == 10 && (Frame[i+2]+Frame[i+3] ==10))
+                {
+                     Total += 30;
+                     break;
+                }
+                else if(Frame[i] == 10 &&( Frame[i + 1] + Frame[i + 2] == 10))
+                {
+                    Total += 20;
+                    break;
+                }
+                else if(Frame[i]+Frame[i+1]==10)
+                {
+                    Total += 10 + Frame[i + 2];
+                    break;
+                }
+                else
+                {
+                    Total += Frame[i] + Frame[i + 1];
+                    break;
+                }
+
+            }
+
+            return Total;
+        }
+
+        
+    }
+}
