6a810ca213edd900878cb45e8375d4682c172fa7
shivamsingh-dev <58263667+shivamsingh-dev@users.noreply.github.com>
2021-02-11 05:59:32 -0800

    Update Game.cs

--- bowling-ball/Game.cs
+++ bowling-ball/Game.cs
@@ -1,17 +1,42 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
 namespace BowlingBall
 {
     public class Game
     {
+        private List<int> _rolls = new List<int>();
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            _rolls.Add(pins);
+
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            var score = 0;
+            var frame_index = 0;
+            for (var frame = 0; frame < 10; frame++)
+            {
+                if (_rolls[frame_index] == 10)
+                {
+                    score += 10 + _rolls[frame_index + 1] + _rolls[frame_index + 2];
+                    frame_index += 1;
+
+                }
+                else if (_rolls[frame_index] + _rolls[frame_index + 1] == 10)
+                {
+                    score += 10 + _rolls[frame_index + 2];
+                    frame_index += 2;
+                }
+                else
+                {
+                    score += _rolls[frame_index] + _rolls[frame_index + 1];
+                    frame_index += 2;
+                }
+            }
+            return score;
+
         }
 
     }
